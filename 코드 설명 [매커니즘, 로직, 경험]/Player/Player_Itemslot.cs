d095bfb7b87ef04406bacfd57e2b177ccea8bbf6
sm99012 <75611409+sm99012@users.noreply.github.com>
2024-05-15 01:39:43 +0900

    Update Player_Itemslot.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Player/Player_Itemslot.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Player/Player_Itemslot.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 public class Player_Itemslot : MonoBehaviour
 {
     // 인벤토리
-    public static Item_Equip[] m_gary_Itemslot_Equip; // 장비아이템 데이터
-    public static int[] m_nary_Itemslot_Equip_Count; // 장비아이템 개수. 장비아이템의 경우 인벤토리 한칸당 1개만 
-    // 장비 아이템 귀속 여부 판단에 이용되는 변수
-    // ㄴ 퀵슬롯 등록시 판매행위 불가.
-    // ㄴ 퀵슬롯 등록시 퀘스트 클리어 불가능.
-    public static bool[] m_bary_Itemslot_Equip_Belong;
-    public static Item_Use[] m_gary_Itemslot_Use;
-    public static int[] m_nary_Itemslot_Use_Count;
-    public static Item_Etc[] m_gary_Itemslot_Etc;
-    public static int[] m_nary_Itemslot_Etc_Count;
-    // 아이템 보유 최대치
-    int m_nMaxCount = 10;
+    public static Item_Equip[] m_gary_Itemslot_Equip; // 장비아이템 데이터. 60칸 제공
+    public static int[] m_nary_Itemslot_Equip_Count; // 장비아이템 개수. 최대 1개 아이템 소지 가능
+    public static bool[] m_bary_Itemslot_Equip_Belong; // 퀵슬롯에 등록된 장비아이템의 각종 제한 판단
+                                                       // 1. 해당 배열의 장비아이템을 퀵슬롯에 등록했다면 NPC와의 거래 불가
+                                                       // 2. 해당 배열의 장비아이템을 퀵슬롯에 등록했다면 해당 배열의 장비아이템을 필요로 하는 퀘스트 클리어 불가(※ 단 퀵슬롯에 등록되지 않은 장비아이템이 존재하는 경우 퀘스트 클리어 가능)
+    public static Item_Use[] m_gary_Itemslot_Use; // 소비아이템 데이터. 60칸 제공
+    public static int[] m_nary_Itemslot_Use_Count; // 소비아이템 개수. 최대 10개 아이템 소지 가능
+    public static Item_Etc[] m_gary_Itemslot_Etc; // 기타아이템 데이터. 60칸 제공
+    public static int[] m_nary_Itemslot_Etc_Count; // 기타아이템 개수. 최대 10개 아이템 소지 가능
 
-    // 보유 재화.
-    public int m_nGold;
+    int m_nMaxCount = 10; // 아이템 보유 최대치
+
+    public int m_nGold;  // 보유 재화
 
 
     public void InitialSet()
@@ -34,21 +32,21 @@ public class Player_Itemslot : MonoBehaviour
         for (int i = 0; i < 60; i++)
         {
             m_bary_Itemslot_Equip_Belong[i] = false;
-            m_nary_Itemslot_Etc_Count[i] = 0;
         }
 
         m_nGold = 0;
     }
 
-    // return 0 ~ 59: 아이템을 획득하여 저장된 배열 넘버.
-    // return -1: 아이템 획득을 하지 못함.
+    // 장비아이템 획득 함수
+    // return 0 ~ 59 : 장비아이템을 획득하여 저장하고 저장된 배열 넘버를 반환한다.
+    // return -1 : 인벤토리에 빈칸이 없어 아이템을 획득하지 못함.
     public int Get_Item_Equip(Item_Equip item)
     {
         for (int i = 0; i < 60; i++)
         {
             if (m_nary_Itemslot_Equip_Count[i] == 0)
             {
-                m_gary_Itemslot_Equip[i] = item;//GetCloneItem(item);
+                m_gary_Itemslot_Equip[i] = item;
                 m_nary_Itemslot_Equip_Count[i] = 1;
 
                 GUIManager_Total.Instance.Update_Quickslot();
@@ -61,11 +59,20 @@ public class Player_Itemslot : MonoBehaviour
 
         return -1;
     }
+    // 소비아이템 획득 함수
+    // return 0 ~ 59 : 소바아이템을 획득하여 저장하고 저장된 배열 넘버를 반환한다.
+    // return -1 : 인벤토리에 빈칸이 없어 아이템을 획득하지 못함.
+    // 소비아이템 획득 매커니즘 : 중복 아이템 획득
+    // 1. 획득할 소비아이템과 동일한 소비아이템이 이미 인벤토리에 존재 하는지 판단한다.
+    // 2. 아이템 보유 최대치(m_nMaxCount)보다 적게 가지고 있는지 판단한다. (Have = true / false)
+    // Have == true : 인벤토리에 이미 존재하는 소비아이템의 배열 넘버의 개수에 +1을 해준다.
+    // Have == false : 인벤토리의 빈칸에 소비아이템을 할당한다.
     public int Get_Item_Use(Item_Use item)
     {
         bool Have = false;
         int arynum_have = -1;
         int arynum_null = -1;
+        // 1. 획득할 소비아이템과 동일한 소비아이템이 이미 인벤토리에 존재 하는지 판단한다.
         for (int i = 0; i < 60; i++)
         {
             if (m_nary_Itemslot_Use_Count[i] == 0)
@@ -77,6 +84,7 @@ public class Player_Itemslot : MonoBehaviour
             }
             else
             {
+                // 2. 아이템 보유 최대치(m_nMaxCount)보다 적게 가지고 있는지 판단한다. (Have = true / false)
                 if (m_gary_Itemslot_Use[i].m_sItemName == item.m_sItemName && m_nary_Itemslot_Use_Count[i] < m_nMaxCount)
                 {
                     Have = true;
@@ -87,6 +95,8 @@ public class Player_Itemslot : MonoBehaviour
                     continue;
             }
         }
+        // 소바아이템을 저장하고 저장된 배열 넘버를 반환한다.
+        // Have == false : 인벤토리의 빈칸에 소비아이템을 할당한다.
         if (Have == false)
         {
             if (m_nary_Itemslot_Use_Count[arynum_null] == 0)
@@ -105,6 +115,7 @@ public class Player_Itemslot : MonoBehaviour
                 return -1;
             }
         }
+        // Have == true : 인벤토리에 이미 존재하는 소비아이템의 배열 넘버의 개수에 +1을 해준다.
         else
         {
             m_nary_Itemslot_Use_Count[arynum_have] += 1;
@@ -114,11 +125,20 @@ public class Player_Itemslot : MonoBehaviour
             return arynum_have;
         }
     }
+    // 기타아이템 획득 함수
+    // return 0 ~ 59 : 기타아이템을 획득하여 저장하고 저장된 배열 넘버를 반환한다.
+    // return -1 : 인벤토리에 빈칸이 없어 아이템을 획득하지 못함.
+    // 기타아이템 획득 매커니즘 : 중복 아이템 획득
+    // 1. 획득할 기타아이템과 동일한 기타아이템이 이미 인벤토리에 존재 하는지 판단한다.
+    // 2. 아이템 보유 최대치(m_nMaxCount)보다 적게 가지고 있는지 판단한다. (Have = true / false)
+    // Have == true : 인벤토리에 이미 존재하는 기타아이템의 배열 넘버의 개수에 +1을 해준다.
+    // Have == false : 인벤토리의 빈칸에 기타아이템을 할당한다.
     public int Get_Item_Etc(Item_Etc item)
     {
         bool Have = false;
         int arynum_have = -1;
         int arynum_null = -1;
+        // 1. 획득할 기타아이템과 동일한 기타아이템이 이미 인벤토리에 존재 하는지 판단한다.
         for (int i = 0; i < 60; i++)
         {
             if (m_nary_Itemslot_Etc_Count[i] == 0)
@@ -130,6 +150,7 @@ public class Player_Itemslot : MonoBehaviour
             }
             else
             {
+                // 2. 아이템 보유 최대치(m_nMaxCount)보다 적게 가지고 있는지 판단한다. (Have = true / false)
                 if (m_gary_Itemslot_Etc[i].m_sItemName == item.m_sItemName && m_nary_Itemslot_Etc_Count[i] < m_nMaxCount)
                 {
                     Have = true;
@@ -140,6 +161,7 @@ public class Player_Itemslot : MonoBehaviour
                     continue;
             }
         }
+        // Have == false : 인벤토리의 빈칸에 기타아이템을 할당한다.
         if (Have == false)
         {
             if (m_nary_Itemslot_Etc_Count[arynum_null] == 0)
@@ -158,6 +180,7 @@ public class Player_Itemslot : MonoBehaviour
                 return -1;
             }
         }
+        // Have == true : 인벤토리에 이미 존재하는 기타아이템의 배열 넘버의 개수에 +1을 해준다.
         else
         {
             m_nary_Itemslot_Etc_Count[arynum_have] += 1;
@@ -168,11 +191,13 @@ public class Player_Itemslot : MonoBehaviour
         }
     }
 
+    // 재화 획득 함수
     public void Get_Gold(int gold)
     {
         m_nGold += gold;
     }
 
+    // 퀘스트 보상으로 장비아이템을 획득하는 함수
     public bool GetQuestReward_Item_Equip(Quest quest)
     {
         for (int i = 0; i < quest.m_lRewardList_Item_Equip.Count; i++)
@@ -186,7 +211,7 @@ public class Player_Itemslot : MonoBehaviour
 
         return false;
     }
-
+    // 퀘스트 보상으로 소비아이템을 획득하는 함수
     public void GetQuestReward_Item_Use(Quest quest)
     {
         for (int i = 0; i < quest.m_lRewardList_Item_Use.Count; i++)
@@ -196,7 +221,7 @@ public class Player_Itemslot : MonoBehaviour
             Get_Item_Use(item);
         }
     }
-
+    // 퀘스트 보상으로 기타아이템을 획득하는 함수
     public void GetQuestReward_Item_Etc(Quest quest)
     {
         for (int i = 0; i < quest.m_lRewardList_Item_Etc.Count; i++)
@@ -207,6 +232,7 @@ public class Player_Itemslot : MonoBehaviour
         }
     }
 
+    // 수집 타입의 퀘스트 클리어에 필요한 아이템을 인벤토리에서 삭제하는 함수
     public void DeleteCollectItem(Quest_COLLECT quest)
     {
         if (quest.m_eQuestType == E_QUEST_TYPE.COLLECT)
@@ -219,6 +245,7 @@ public class Player_Itemslot : MonoBehaviour
                     {
                         if (m_gary_Itemslot_Equip[a].m_nItemCode == quest.m_nl_ItemCode[i])
                         {
+                            // 장비아이템이 퀵슬롯에 등록된 경우 수집 타입의 퀘스트 클리어에 필요한 아이템이라 인정되지 않음.
                             if (m_bary_Itemslot_Equip_Belong[a] == false)
                             {
                                 m_nary_Itemslot_Equip_Count[a] -= quest.m_nl_ItemCount_Max[i];
@@ -249,6 +276,7 @@ public class Player_Itemslot : MonoBehaviour
         }
     }
 
+    // 아이템의 원본 데이터를 반환
     public GameObject GetCloneItem(GameObject item)
     {
         GameObject ReturnITem;
@@ -268,8 +296,7 @@ public class Player_Itemslot : MonoBehaviour
         return ReturnITem;
     }
 
-    // 아이템 퀵슬롯 등록 관련.
-    // 퀵슬롯에 등록된 아이템의 총 수량 및 사용.
+    // 퀵슬롯에 등록된 아이템의 수량 현황을 확인하는 함수
     public int Check_Quickslot_Item(E_QUICKSLOT_CATEGORY eqc, int itemcode)
     {
         int itemcount = 0;
@@ -307,12 +334,12 @@ public class Player_Itemslot : MonoBehaviour
 
         return itemcount;
     }
-    // 퀵슬롯에 등록된 아이템 사용.
+    // 퀵슬롯에 등록된 아이템을 사용하는 함수. 아이템의 수량 현황을 갱신하는 용도
     public void Use_Quickslot_Item(E_QUICKSLOT_CATEGORY eqc, int itemcode)
     {
         if (eqc == E_QUICKSLOT_CATEGORY.EQUIP)
         {
-            // 사용한다고 사라지는게 아님.
+            // 인벤토리와 퀵슬롯의 장비아이템은 수량이 1로 고정되어있다.
         }
         else if (eqc == E_QUICKSLOT_CATEGORY.USE)
         {
@@ -334,23 +361,17 @@ public class Player_Itemslot : MonoBehaviour
         }
         else if (eqc == E_QUICKSLOT_CATEGORY.ETC)
         {
-            // 사용 불가.
+            // 기타아이템은 사용이 불가능하다.
         }
     }
 
-    // 리트라이 시 지불할 골드.
+    // 리트라이(부활) 시 지불할 재화.
     public void ReTry_Pay_Gold(int gold)
     {
         m_nGold -= gold;
     }
-    // 리트라이 시 사라지는 골드.
-    public bool ReTry_Lost_Gold(int gold)
-    {
 
-
-        return true;
-    }
-    // 리트라이 시 사라지는 아이템.
+    // 리트라이(부활) 시 잃어버리는 장비아이템에 관한 함수.
     public bool ReTry_Lost_Item_Equip(Dictionary<int, int> dictionary)
     {
         foreach(KeyValuePair<int, int> item in dictionary)
@@ -361,6 +382,7 @@ public class Player_Itemslot : MonoBehaviour
 
         return true;
     }
+    // 장비아이템을 삭제하는 함수. itemnumber는 아이템이 가지는 고유 코드(아이템 생성 순서)
     void Delete_Item_Equip(int itemnumber)
     {
         for (int i = 0; i < m_gary_Itemslot_Equip.Length; i++)
@@ -377,6 +399,7 @@ public class Player_Itemslot : MonoBehaviour
             }
         }
     }
+    // 리트라이(부활) 시 잃어버리는 소비아이템에 관한 함수.
     public bool ReTry_Lost_Item_Use(Dictionary<int, int> dictionary)
     {
         foreach (KeyValuePair<int, int> item in dictionary)
@@ -387,32 +410,11 @@ public class Player_Itemslot : MonoBehaviour
 
         return true;
     }
+    // 소비아이템을 삭제하는 함수. itemcode가 동일한 소비아이템을 Itemcount 만큼 삭제한다.
     void Delete_Item_Use(int itemcode, int itemcount)
     {
         int ic = itemcount;
-        //for (int i = 0; i < m_gary_Itemslot_Use.Length;)
-        //{
-        //    if (m_nary_Itemslot_Use_Count[i] > 0)
-        //    {
-        //        if (m_gary_Itemslot_Use[i].m_nItemCode == itemcode)
-        //        {
-        //            itemcount -= 1;
-        //            m_nary_Itemslot_Use_Count[i] -= 1;
-
-        //            if (m_nary_Itemslot_Use_Count[i] < 1)
-        //                i++;
-
-        //            if (itemcount < 1)
-        //            {
-        //                break;
-        //            }
-        //        }
-        //        else
-        //            i++;
-        //    }
-        //    else
-        //        i++;
-        //}
+        
         for (int i = m_gary_Itemslot_Use.Length - 1; i >= 0;)
         {
             if (m_nary_Itemslot_Use_Count[i] > 0)
@@ -440,6 +442,7 @@ public class Player_Itemslot : MonoBehaviour
                 i--;
         }
     }
+    // 리트라이(부활) 시 잃어버리는 기타아이템에 관한 함수.
     public bool ReTry_Lost_Item_Etc(Dictionary<int, int> dictionary)
     {
         foreach (KeyValuePair<int, int> item in dictionary)
@@ -450,6 +453,7 @@ public class Player_Itemslot : MonoBehaviour
 
         return true;
     }
+    // 기타아이템을 삭제하는 함수. itemcode가 동일한 기타아이템을 Itemcount 만큼 삭제한다.
     void Delete_Item_Etc(int itemcode, int itemcount)
     {
         int ic = itemcount;
@@ -480,6 +484,8 @@ public class Player_Itemslot : MonoBehaviour
                 i--;
         }
     }
+
+    // 리트라이(부활) 시 잃어버리는 재화. 리트라이 단계에 따라 잃어버리는 재화의 비율이 결정된다. 
     public int ReTry_Lost_Gold(int min = 1, int max = 10)
     {
         int lostrate = Random.Range(min, max + 1);
@@ -490,9 +496,8 @@ public class Player_Itemslot : MonoBehaviour
         return lostgold;
     }
 
-    // 기프트 사용 관련 함수들.
-    // 장비 아이템 슬롯의 잔여 슬롯 확인.
-    // itemcount 만큼의 장비 아이템을 획득할 여분의 장비 아이템 슬롯이 남아 있는지 판단.
+    // 소비아이템_기프트(랜덤박스, 선물상자 등) 사용 관련 함수들.
+    // 인벤토리(장비아이템)의 남은 공간 확인. itemcount 만큼의 소비아이템을 획득할 여분의 인벤토리 공간이 남아 있는지 판단.
     public bool Check_Get_Item_Itemslot_Equip(int itemcount)
     {
         int value = 0;
@@ -507,8 +512,7 @@ public class Player_Itemslot : MonoBehaviour
         else
             return false;
     }
-    // 소비 아이템 슬롯의 잔여 슬롯 확인.
-    // itemcount 만큼의 소비 아이템을 획득할 여분의 소비 아이템 슬롯이 남아 있는지 판단.
+    // 인벤토리(소비아이템)의 남은 공간 확인. itemcount 만큼의 소비아이템을 획득할 여분의 인벤토리 공간이 남아 있는지 판단.
     public bool Check_Get_Item_Itemslot_Use(int itemcount)
     {
         int value = 0;
@@ -559,8 +563,7 @@ public class Player_Itemslot : MonoBehaviour
 
         return true;
     }
-    // 기타 아이템 슬롯의 잔여 슬롯 확인.
-    // itemcount 만큼의 기타 아이템을 획득할 여분의 기타 아이템 슬롯이 남아 있는지 판단.
+    // 인벤토리(기타아이템)의 남은 공간 확인. itemcount 만큼의 기타아이템을 획득할 여분의 인벤토리 공간이 남아 있는지 판단.
     public bool Check_Get_Item_Itemslot_Etc(int itemcount)
     {
         int value = 0;
