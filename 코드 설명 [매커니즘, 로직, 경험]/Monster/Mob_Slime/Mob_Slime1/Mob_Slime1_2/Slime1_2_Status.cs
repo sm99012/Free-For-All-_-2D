7fa82afa85652ee61838f5ee3ee355db8d2312b5
sm99012 <75611409+sm99012@users.noreply.github.com>
2024-05-17 17:48:24 +0900

    Add files via upload

---
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime1/Mob_Slime1_2/Slime1_2_Status.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime1_2_Status : Slime1_Status
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_eObject_State = E_OBJECT_STATE.MONSTER;
+        m_eMonster_Kind = E_MONSTER_KIND.SLIME;
+
+        InitialSet_SOC();
+        InitialSet_STATUS();
+
+        m_sMonsterName = "초원 슬라임";
+        m_nMonsterCode = 2000;
+    }
+}
