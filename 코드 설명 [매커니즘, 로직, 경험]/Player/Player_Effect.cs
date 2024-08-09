65dd9dba6ea2ecf8fd38e97a39da4605ae95156e
배종원 <75611409+sm99012@users.noreply.github.com>
2024-08-09 19:45:41 +0900

    Update Player_Effect.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Player/Player_Effect.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Player/Player_Effect.cs
@@ -8,13 +8,12 @@ public class Player_Effect : MonoBehaviour
 
     public static Dictionary<int, Skill_Effect> m_sDictionary_Skill_Effect_Apply; // 플레이어에게 적용중인 스킬 이펙트 딕셔너리
 
-    GameObject m_gCondition;
-
-    public GameObject m_gCondition_Bind;    // 속박 이펙트(애니메이션) 10000
-    public GameObject m_gCondition_Shock;   // 기절 이펙트(애니메이션) 11000
-    public GameObject m_gCondition_Dark;    // 암흑 이펙트(애니메이션) 12000
-    public GameObject m_gCondition_Slow;    // 둔화 이펙트(애니메이션) 13000
-    public GameObject m_gCondition_Confuse; // 혼란 이펙트(애니메이션) 14000
+    GameObject m_gCondition; // 플레이어 스킬(상태이상) 오브젝트
+    public GameObject m_gCondition_Bind;    // 속박 이펙트(애니메이션) 오브젝트
+    public GameObject m_gCondition_Shock;   // 기절 이펙트(애니메이션) 오브젝트
+    public GameObject m_gCondition_Dark;    // 암흑 이펙트(애니메이션) 오브젝트
+    public GameObject m_gCondition_Slow;    // 둔화 이펙트(애니메이션) 오브젝트
+    public GameObject m_gCondition_Confuse; // 혼란 이펙트(애니메이션) 오브젝트
 
     public void InitialSet()
     {
@@ -116,14 +115,14 @@ public class Player_Effect : MonoBehaviour
         }
     }
 
-    // 스킬 적용 시 이펙트 연출
+    // 스킬(버프ㆍ디버프, 상태이상) 적용 시 이펙트 연출
     // return true : 이펙트 연출 / return false : 이펙트 미연출
     public bool ApplySkill(int ncode, Skill_Effect ske) // ncode : 적용할 스킬코드, skse : 스킬 이펙트
     {
         if (m_sDictionary_Skill_Effect_Apply.ContainsKey(ncode) == false)
-            m_sDictionary_Skill_Effect_Apply.Add(ncode, ske);
+            m_sDictionary_Skill_Effect_Apply.Add(ncode, ske); // 플레이어에게 적용중인 스킬 이펙트 딕셔너리에 해당 스킬 이펙트 추가
 
-        ApplySkill_Condition_Effect();
+        ApplySkill_Condition_Effect(); // 스킬(상태이상) 적용 시 이펙트 연출
 
         if (ske.m_gSkill_Effect != null)
             return true;
@@ -134,46 +133,44 @@ public class Player_Effect : MonoBehaviour
     public void ApplySkill_Condition_Effect()
     {
         if (Player_Skill.m_Skill_Condition.ConditionCheck_Bind() == true)
-            m_gCondition_Bind.SetActive(true);    // 상태이상(속박) 이팩트 활성화
+            m_gCondition_Bind.SetActive(true);    // 상태이상(속박) 이펙트 활성화
         if (Player_Skill.m_Skill_Condition.ConditionCheck_Shock() == true)
-            m_gCondition_Shock.SetActive(true);   // 상태이상(기절) 이팩트 활성화
+            m_gCondition_Shock.SetActive(true);   // 상태이상(기절) 이펙트 활성화
         if (Player_Skill.m_Skill_Condition.ConditionCheck_Dark() == true)
-            m_gCondition_Dark.SetActive(true);    // 상태이상(암흑) 이팩트 활성화
+            m_gCondition_Dark.SetActive(true);    // 상태이상(암흑) 이펙트 활성화
         if (Player_Skill.m_Skill_Condition.ConditionCheck_Slow() == true)
-            m_gCondition_Slow.SetActive(true);    // 상태이상(둔화) 이팩트 활성화
+            m_gCondition_Slow.SetActive(true);    // 상태이상(둔화) 이펙트 활성화
         if (Player_Skill.m_Skill_Condition.ConditionCheck_Confuse() == true)
-            m_gCondition_Confuse.SetActive(true); // 상태이상(혼란) 이팩트 활성화
+            m_gCondition_Confuse.SetActive(true); // 상태이상(혼란) 이펙트 활성화
     }
 
-    // 스킬(버프ㆍ디버프) 해제 시 이펙트 업데이트
+    // 스킬(버프ㆍ디버프) 해제 시 이펙트 제거
     public void UnApplySkill(int ncode) // ncode : 해제할 스킬(버프ㆍ디버프)코드
     {
         m_sDictionary_Skill_Effect_Apply.Remove(ncode);
     }
-    // 스킬(상태이상) 해제 시 이펙트 업데이트
+    // 스킬(상태이상) 해제 시 이펙트 비활성화
     public void UnApplySkill_Condition_Effect(int ncode) // ncode : 해제할 스킬(상태이상)코드
     {
-        //Debug.Log("UnApplySkill");
-
         if (ncode / 1000 == 10)
-            m_gCondition_Bind.SetActive(false);    // 상태이상(속박) 이팩트 비활성화
+            m_gCondition_Bind.SetActive(false);    // 스킬(상태이상[속박]) 이펙트 비활성화
         if (ncode / 1000 == 11)
-            m_gCondition_Shock.SetActive(false);   // 상태이상(기절) 이팩트 비활성화
+            m_gCondition_Shock.SetActive(false);   // 스킬(상태이상[기절]) 이펙트 비활성화
         if (ncode / 1000 == 12)
-            m_gCondition_Dark.SetActive(false);    // 상태이상(암흑) 이팩트 비활성화
+            m_gCondition_Dark.SetActive(false);    // 스킬(상태이상[암흑]) 이펙트 비활성화
         if (ncode / 1000 == 13)
-            m_gCondition_Slow.SetActive(false);    // 상태이상(둔화) 이팩트 비활성화
+            m_gCondition_Slow.SetActive(false);    // 스킬(상태이상[둔화]) 이펙트 비활성화
         if (ncode / 1000 == 14)
-            m_gCondition_Confuse.SetActive(false); // 상태이상(혼란) 이팩트 비활성화
+            m_gCondition_Confuse.SetActive(false); // 스킬(상태이상[혼란]) 이펙트 비활성화
     }
 
-    // 리트라이(부활) 시 플레이어에게 적용중인 이펙트 초기화
+    // 리트라이(부활) 시 플레이어에게 적용중인 스킬(상태이상) 이펙트 초기화
     public void ReTry()
     {
-        m_gCondition_Bind.SetActive(false);    // 상태이상(속박) 이팩트 비활성화
-        m_gCondition_Shock.SetActive(false);   // 상태이상(기절) 이팩트 비활성화
-        m_gCondition_Dark.SetActive(false);    // 상태이상(암흑) 이팩트 비활성화
-        m_gCondition_Slow.SetActive(false);    // 상태이상(둔화) 이팩트 비활성화
-        m_gCondition_Confuse.SetActive(false); // 상태이상(혼란) 이팩트 비활성화
+        m_gCondition_Bind.SetActive(false);    // 스킬(상태이상[속박]) 이펙트 비활성화
+        m_gCondition_Shock.SetActive(false);   // 스킬(상태이상[기절]) 이펙트 비활성화
+        m_gCondition_Dark.SetActive(false);    // 스킬(상태이상[암흑]) 이펙트 비활성화
+        m_gCondition_Slow.SetActive(false);    // 스킬(상태이상[둔화] 이펙트 비활성화
+        m_gCondition_Confuse.SetActive(false); // 스킬(상태이상[혼란]) 이펙트 비활성화
     }
 }
