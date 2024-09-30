91c40255d76e8b933159e37efaef73c550b7e88b
배종원 <75611409+sm99012@users.noreply.github.com>
2024-09-30 12:23:20 +0900

    Update ItemManagerSlot.cs

--- 코드 설명 [매커니즘, 로직, 경험]/GUI/GUI_ItemManager/ItemManagerSlot.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/GUI/GUI_ItemManager/ItemManagerSlot.cs
@@ -9,32 +9,32 @@ public class ItemManagerSlot : MonoBehaviour, IPointerClickHandler
 {
     public Image m_IMG_ItemManager_Slot_Sprite;
 
-    [SerializeField] ItemType m_eItemtype;
+    [SerializeField] E_ITEM_TYPE m_eItemType;
     [SerializeField] int m_nItemCode;
 
     public void SetItemInformation(Item item)
     {
         m_IMG_ItemManager_Slot_Sprite.color = new Color(1, 1, 1, 1);
         m_IMG_ItemManager_Slot_Sprite.sprite = item.m_sp_Sprite;
-        m_eItemtype = item.m_eItemtype;
+        m_eItemType = item.m_eItemType;
         m_nItemCode = item.m_nItemCode;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     { 
-        switch(m_eItemtype)
+        switch(m_eItemType)
         {
-            case ItemType.EQUIP:
+            case E_ITEM_TYPE.EQUIP:
                 {
                     Item item = new Item_Equip(ItemManager.instance.m_Dictionary_MonsterDrop_Equip[m_nItemCode], Player_Total.Instance.gameObject.transform.position);
                     Destroy(item);
                 } break;
-            case ItemType.USE:
+            case E_ITEM_TYPE.USE:
                 {
                     Item item = new Item_Use(ItemManager.instance.m_Dictionary_MonsterDrop_Use[m_nItemCode], Player_Total.Instance.gameObject.transform.position);
                     Destroy(item);
                 } break;
-            case ItemType.ETC:
+            case E_ITEM_TYPE.ETC:
                 {
                     Item item = new Item_Etc(ItemManager.instance.m_Dictionary_MonsterDrop_Etc[m_nItemCode], Player_Total.Instance.gameObject.transform.position);
                     Destroy(item);
