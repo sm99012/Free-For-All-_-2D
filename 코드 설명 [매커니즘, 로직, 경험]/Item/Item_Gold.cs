5c29e533eb2006fb39b84554f8cfcfd3cda99ea9
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-04 11:05:26 +0900

    Update Item_Gold.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Item/Item_Gold.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Item/Item_Gold.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Item_Gold : Item
 {
-    public int m_nGold;
+    public int m_nGold; // 골드(재화)
 
+    // 빈 생성자
     public Item_Gold() { }
-
-    // Item 생성.(골드)
-    public Item_Gold(int gold, Vector3 itemposition)
+    // 골드(재화)를 생성하는 생성자. 필드에 골드(재화) 드랍 시 사용되는 생성자
+    public Item_Gold(int gold, Vector3 itemposition) // gold : 획득할 골드(재화), itemposition : 골드(재화) 드랍 위치
     {
-        GameObject itemobject = Instantiate(ItemManager.instance.m_gItem_Gold_Null);
+        GameObject itemobject = Instantiate(ItemManager.instance.m_gItem_Gold_Null); // 골드(재화) 사본(유니티 오브젝트) 생성
+
+        // 골드(재화) 사본 객체 데이터 할당
         Item_Gold itemscript = itemobject.GetComponent<Item_Gold>();
 
         itemscript.m_bPossible_Get = false;
@@ -19,8 +21,8 @@ public class Item_Gold : Item
         itemscript.m_sItemName = gold.ToString() + " 골드";
         itemscript.m_sItemDescription = gold.ToString() + " 골드";
         itemscript.m_nItemCode = 0;
-        //itemscript.m_nItemNumber = ++ItemManager.sm_nItemNumber;
-        itemscript.m_nItemNumber = 0;
+        itemscript.m_nItemNumber = 0; // 골드(재화)의 경우 아이템 생성코드 미할당(0 고정)
+        // 획득할 골드(재화)의 양에 따라 아이템 스프라이트(이미지) 설정
         if (gold < 1000)
             itemscript.m_sp_Sprite = Resources.Load<Sprite>("Prefab/Item/Item_Gold/Gold Nugget");
         else
@@ -37,19 +39,18 @@ public class Item_Gold : Item
 
         itemobject.name = gold.ToString() + " 골드";
 
-        Debug.Log("ItemName: " + itemscript.m_sItemName + ", ItemNumber: " + itemscript.m_nItemNumber);
-
         itemscript.m_FadeinAlpa = 0;
-        itemscript.Fadein();
+        itemscript.Fadein(); // 아이템 생성 시 페이드인 효과 실행 함수
     }
 
-    // Item 획득.(골드)
+    // 플레이어가 골드(재화) 사본을 획득할 시 사용되는 함수
+    // 골드(재화) 사본(유니티 오브젝트)을 획득할 시 해당 골드(재화) 사본(유니티 오브젝트)은 삭제되고 해당 골드(재화) 사본(메모리 객체) 데이터(단순 int 타입 데이터)를 저장소에 저장
     public int GetGold(Item_Gold item)
     {
         int gold = item.m_nPrice;
+        
         Destroy(this.gameObject);
 
         return gold;
-        //return item.m_nGold;
     }
 }
