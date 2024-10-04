4283a3be6948d9857c5b846f82f68e0cd7e85836
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-04 09:53:29 +0900

    Update Item_Equip.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Item/Item_Equip.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Item/Item_Equip.cs
@@ -38,7 +38,7 @@ public class Item_Equip : Item // 기반이 되는 Item 클래스 상속
     // 생성자 오버로딩을 이용한 장비아이템 생성 함수(경우에 따라 사용하는 생성자가 다르다.)
     // 빈 생성자
     public Item_Equip() { }
-    // 장비아이템 원본을 생성하는 생성자. 게임 시작시 최초 1회만 실행되는 함수
+    // 장비아이템 원본을 생성하는 생성자. 게임 시작 시 최초 1회만 사용되는 생성자
     public Item_Equip(string name, int code, string path_sprite,
         E_ITEM_GRADE eig, E_ITEM_EQUIP_TYPE iet,
         E_ITEM_EQUIP_MAINWEAPON_TYPE iemt = E_ITEM_EQUIP_MAINWEAPON_TYPE.NULL,
@@ -72,7 +72,7 @@ public class Item_Equip : Item // 기반이 되는 Item 클래스 상속
         m_nPrice = price;
         m_nItemSetCode = setitemcode;
     }
-    // 장비아이템 사본을 생성하는 생성자. 필드에 장비아이템 드랍 시 실행되는 함수
+    // 장비아이템 사본을 생성하는 생성자. 필드에 장비아이템 드랍 시 사용되는 생성자
     public Item_Equip(Item_Equip item, Vector3 itemposition) // item : 장비아이템 원본 객체, itemposition : 장비아이템 드랍 위치
     {
         GameObject itemobject = Instantiate(ItemManager.instance.m_gItem_Equip_Null); // 장비아이템 사본(유니티 오브젝트) 생성
@@ -128,7 +128,7 @@ public class Item_Equip : Item // 기반이 되는 Item 클래스 상속
         itemscript.m_FadeinAlpa = 0;
         itemscript.Fadein(); // 아이템 생성시 페이드인 효과 실행 함수
     }
-    // 장비아이템 사본을 생성하는 생성자. 플레이어가 장비아이템 사본을 획득할 시 실행되는 함수
+    // 장비아이템 사본을 생성하는 생성자. 플레이어가 장비아이템 사본을 획득할 시 사용되는 생성자
     // 장비아이템 사본(유니티 오브젝트)을 획득할 시 해당 장비아이템 사본(유니티 오브젝트)은 삭제되고 해당 장비아이템 사본(메모리 객체) 데이터를 저장소에 저장
     public Item_Equip DeleteItem(Item_Equip item) // item : 장비아이템 사본 객체
     {
@@ -175,7 +175,7 @@ public class Item_Equip : Item // 기반이 되는 Item 클래스 상속
 
         return itemscript; // 장비아이템 사본(메모리 객체) 데이터 반환
     }
-    // 장비아이템 사본을 생성하는 생성자. 플레이어가 장비아이템 사본을 획득할 시 실행되는 함수
+    // 장비아이템 사본을 생성하는 생성자. 플레이어가 장비아이템 사본을 획득할 시 사용되는 생성자
     // 퀘스트 완료 보상으로 획득하는 장비아이템의 경우 장비아이템 사본(유니티 오브젝트)이 필요하지 않아 바로 장비아이템 사본(메모리 객체) 데이터를 저장소에 저장
     public Item_Equip CreateItem(Item_Equip item)
     {
