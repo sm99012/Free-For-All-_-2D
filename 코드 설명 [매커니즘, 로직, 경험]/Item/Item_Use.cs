6495b236dcfaf64a7cf986d5813535fddb85f23a
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-04 10:25:06 +0900

    Update Item_Use.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Item/Item_Use.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Item/Item_Use.cs
@@ -88,105 +88,6 @@ public class Item_Use : Item // 기반이 되는 Item 클래스 상속
         
         this.m_nPrice = price;
     }
-
-    // 소비아이템(기프트) 관련 설정 함수
-    public void Set_Item_Use_Gift(E_ITEM_USE_GIFT_TYPE eiugt, int pickcount_min = 0, int pickcount_max = 0) // eiugt : 소비아이템(기프트) 타입
-    {
-        m_eItemUseGiftType = eiugt;
-        m_nRandomBox_PickCount_Min = pickcount_min;
-        m_nRandomBox_PickCount_Max = pickcount_max;
-    }
-    // 소비아이템(기프트) 목록 추가 함수
-    public void Add_Gift_Item_Equip(int code, int count, int probability = 10000) // code : 추가할 장비아이템 고유코드, count : 아이템 획득 개수, probability : 아이템 획득 확률
-    {
-        m_nDictionary_Gift_Item_Equip_Code.Add(m_nDictionary_Gift_Item_Equip_Probability.Count, code);
-        m_nDictionary_Gift_Item_Equip_Count.Add(m_nDictionary_Gift_Item_Equip_Probability.Count, count);
-        m_nDictionary_Gift_Item_Equip_Probability.Add(m_nDictionary_Gift_Item_Equip_Probability.Count, probability);
-    }
-    public void Add_Gift_Item_Use(int code, int count, int probability = 10000) // code : 추가할 소비아이템 고유코드, count : 아이템 획득 개수, probability : 아이템 획득 확률
-    {
-        m_nDictionary_Gift_Item_Use_Code.Add(m_nDictionary_Gift_Item_Use_Probability.Count, code);
-        m_nDictionary_Gift_Item_Use_Count.Add(m_nDictionary_Gift_Item_Use_Probability.Count, count);
-        m_nDictionary_Gift_Item_Use_Probability.Add(m_nDictionary_Gift_Item_Use_Probability.Count, probability);
-    }
-    public void Add_Gift_Item_Etc(int code, int count, int probability = 10000) // code : 추가할 기타아이템 고유코드, count : 아이템 획득 개수, probability : 아이템 획득 확률
-    {
-        m_nDictionary_Gift_Item_Etc_Code.Add(m_nDictionary_Gift_Item_Etc_Probability.Count, code);
-        m_nDictionary_Gift_Item_Etc_Count.Add(m_nDictionary_Gift_Item_Etc_Probability.Count, count);
-        m_nDictionary_Gift_Item_Etc_Probability.Add(m_nDictionary_Gift_Item_Etc_Probability.Count, probability);
-    }
-
-    // 소비아이템(기프트) 사용 정보(아이템 목록, 아이템 획득 개수, 아이템 획득 확률) 반환 함수
-    public string Return_Gift_List()
-    {
-        string strtoken = "";
-        if (m_eItemUseGiftType == E_ITEM_USE_GIFT_TYPE.FIXEDBOX)
-        {
-            strtoken += "[사용 시 아래 아이템을 모두 획득합니다.]\n";
-            for (int i = 0; i < m_nDictionary_Gift_Item_Equip_Code.Count; i++)
-            {
-                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Equip[m_nDictionary_Gift_Item_Equip_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Equip_Count[i] + " 개\n";
-                //strtoken += m_nDictionary_Gift_Item_Equip_Code[i] + " " + m_nDictionary_Gift_Item_Equip_Count[i] + " 개\n";
-
-            }
-            for (int i = 0; i < m_nDictionary_Gift_Item_Use_Code.Count; i++)
-            {
-                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Use[m_nDictionary_Gift_Item_Use_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Use_Count[i] + " 개\n";
-                //strtoken += m_nDictionary_Gift_Item_Use_Code[i] + " " + m_nDictionary_Gift_Item_Use_Count[i] + " 개\n";
-            }
-            for (int i = 0; i < m_nDictionary_Gift_Item_Etc_Code.Count; i++)
-            {
-                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Etc[m_nDictionary_Gift_Item_Etc_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Etc_Count[i] + " 개\n";
-                //strtoken += m_nDictionary_Gift_Item_Etc_Code[i] + " " + m_nDictionary_Gift_Item_Etc_Count[i] + " 개\n";
-
-            }
-
-            return strtoken;
-        }
-        else if (m_eItemUseGiftType == E_ITEM_USE_GIFT_TYPE.RANDOMBOX_INDEPENDENTTRIAL)
-        {
-            strtoken += "[사용 시 아래 아이템을 획득합니다.]\n";
-            strtoken += "[획득할 수 있는 아이템 개수: " + m_nRandomBox_PickCount_Min.ToString() + " ~ " + m_nRandomBox_PickCount_Max.ToString() + " 개.]\n";
-            strtoken += "[아이템 중복 획득이 가능합니다.]\n";
-            for (int i = 0; i < m_nDictionary_Gift_Item_Equip_Code.Count; i++)
-            {
-                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Equip[m_nDictionary_Gift_Item_Equip_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Equip_Count[i] + " 개 " + Mathf.Round(((float)(m_nDictionary_Gift_Item_Equip_Probability[i] / (float)10000) * 100)) + " %\n";
-            }
-            for (int i = 0; i < m_nDictionary_Gift_Item_Use_Code.Count; i++)
-            {
-                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Use[m_nDictionary_Gift_Item_Use_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Use_Count[i] + " 개 " + Mathf.Round(((float)(m_nDictionary_Gift_Item_Use_Probability[i] / (float)10000) * 100)) + " %\n";
-            }
-            for (int i = 0; i < m_nDictionary_Gift_Item_Etc_Code.Count; i++)
-            {
-                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Etc[m_nDictionary_Gift_Item_Etc_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Etc_Count[i] + " 개 " + Mathf.Round(((float)(m_nDictionary_Gift_Item_Etc_Probability[i] / (float)10000) * 100)) + " %\n";
-            }
-
-            return strtoken;
-        }
-        else if (m_eItemUseGiftType == E_ITEM_USE_GIFT_TYPE.RANDOMBOX_DEPENDENTTRIAL)
-        {
-            strtoken += "[사용 시 아래 아이템을 획득합니다.]\n";
-            strtoken += "[획득할 수 있는 아이템 개수: " + m_nRandomBox_PickCount_Min.ToString() + " ~ " + m_nRandomBox_PickCount_Max.ToString() + " 개.]\n";
-            strtoken += "[아이템 중복 획득이 불가능합니다.]\n";
-            for (int i = 0; i < m_nDictionary_Gift_Item_Equip_Code.Count; i++)
-            {
-                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Equip[m_nDictionary_Gift_Item_Equip_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Equip_Count[i] + " 개 " + Mathf.Round(((float)(m_nDictionary_Gift_Item_Equip_Probability[i] / (float)10000) * 100)) + " %\n";
-            }
-            for (int i = 0; i < m_nDictionary_Gift_Item_Use_Code.Count; i++)
-            {
-                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Use[m_nDictionary_Gift_Item_Use_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Use_Count[i] + " 개 " + Mathf.Round(((float)(m_nDictionary_Gift_Item_Use_Probability[i] / (float)10000) * 100)) + " %\n";
-            }
-            for (int i = 0; i < m_nDictionary_Gift_Item_Etc_Code.Count; i++)
-            {
-                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Etc[m_nDictionary_Gift_Item_Etc_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Etc_Count[i] + " 개 " + Mathf.Round(((float)(m_nDictionary_Gift_Item_Etc_Probability[i] / (float)10000) * 100)) + " %\n";
-            }
-
-            return strtoken;
-        }
-        else
-            return "";
-    }
-
     // Item 사본.
     public Item_Use(Item_Use item, Vector3 itemposition)
     {
@@ -389,5 +290,103 @@ public class Item_Use : Item // 기반이 되는 Item 클래스 상속
 
         return itemscript;
 
+    }
+
+        // 소비아이템(기프트) 관련 설정 함수
+    public void Set_Item_Use_Gift(E_ITEM_USE_GIFT_TYPE eiugt, int pickcount_min = 0, int pickcount_max = 0) // eiugt : 소비아이템(기프트) 타입
+    {
+        m_eItemUseGiftType = eiugt;
+        m_nRandomBox_PickCount_Min = pickcount_min;
+        m_nRandomBox_PickCount_Max = pickcount_max;
+    }
+    // 소비아이템(기프트) 목록 추가 함수
+    public void Add_Gift_Item_Equip(int code, int count, int probability = 10000) // code : 추가할 장비아이템 고유코드, count : 아이템 획득 개수, probability : 아이템 획득 확률
+    {
+        m_nDictionary_Gift_Item_Equip_Code.Add(m_nDictionary_Gift_Item_Equip_Probability.Count, code);
+        m_nDictionary_Gift_Item_Equip_Count.Add(m_nDictionary_Gift_Item_Equip_Probability.Count, count);
+        m_nDictionary_Gift_Item_Equip_Probability.Add(m_nDictionary_Gift_Item_Equip_Probability.Count, probability);
+    }
+    public void Add_Gift_Item_Use(int code, int count, int probability = 10000) // code : 추가할 소비아이템 고유코드, count : 아이템 획득 개수, probability : 아이템 획득 확률
+    {
+        m_nDictionary_Gift_Item_Use_Code.Add(m_nDictionary_Gift_Item_Use_Probability.Count, code);
+        m_nDictionary_Gift_Item_Use_Count.Add(m_nDictionary_Gift_Item_Use_Probability.Count, count);
+        m_nDictionary_Gift_Item_Use_Probability.Add(m_nDictionary_Gift_Item_Use_Probability.Count, probability);
+    }
+    public void Add_Gift_Item_Etc(int code, int count, int probability = 10000) // code : 추가할 기타아이템 고유코드, count : 아이템 획득 개수, probability : 아이템 획득 확률
+    {
+        m_nDictionary_Gift_Item_Etc_Code.Add(m_nDictionary_Gift_Item_Etc_Probability.Count, code);
+        m_nDictionary_Gift_Item_Etc_Count.Add(m_nDictionary_Gift_Item_Etc_Probability.Count, count);
+        m_nDictionary_Gift_Item_Etc_Probability.Add(m_nDictionary_Gift_Item_Etc_Probability.Count, probability);
+    }
+
+    // 소비아이템(기프트) 사용 정보(아이템 목록, 아이템 획득 개수, 아이템 획득 확률) 반환 함수
+    public string Return_Gift_List()
+    {
+        string strtoken = "";
+        // 1. 소비아이템(기프트(아이템 확정 지급형))
+        if (m_eItemUseGiftType == E_ITEM_USE_GIFT_TYPE.FIXEDBOX)
+        {
+            strtoken += "[사용 시 아래 아이템을 모두 획득합니다.]\n";
+            for (int i = 0; i < m_nDictionary_Gift_Item_Equip_Code.Count; i++)
+            {
+                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Equip[m_nDictionary_Gift_Item_Equip_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Equip_Count[i] + " 개\n";
+
+            }
+            for (int i = 0; i < m_nDictionary_Gift_Item_Use_Code.Count; i++)
+            {
+                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Use[m_nDictionary_Gift_Item_Use_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Use_Count[i] + " 개\n";
+            }
+            for (int i = 0; i < m_nDictionary_Gift_Item_Etc_Code.Count; i++)
+            {
+                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Etc[m_nDictionary_Gift_Item_Etc_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Etc_Count[i] + " 개\n";
+
+            }
+
+            return strtoken;
+        }
+        // 2. 소비아이템(기프트(랜덤 지급형 _ A 타입))
+        else if (m_eItemUseGiftType == E_ITEM_USE_GIFT_TYPE.RANDOMBOX_INDEPENDENTTRIAL)
+        {
+            strtoken += "[사용 시 아래 아이템을 획득합니다.]\n";
+            strtoken += "[획득할 수 있는 아이템 개수: " + m_nRandomBox_PickCount_Min.ToString() + " ~ " + m_nRandomBox_PickCount_Max.ToString() + " 개.]\n";
+            strtoken += "[아이템 중복 획득이 가능합니다.]\n";
+            for (int i = 0; i < m_nDictionary_Gift_Item_Equip_Code.Count; i++)
+            {
+                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Equip[m_nDictionary_Gift_Item_Equip_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Equip_Count[i] + " 개 " + Mathf.Round(((float)(m_nDictionary_Gift_Item_Equip_Probability[i] / (float)10000) * 100)) + " %\n";
+            }
+            for (int i = 0; i < m_nDictionary_Gift_Item_Use_Code.Count; i++)
+            {
+                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Use[m_nDictionary_Gift_Item_Use_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Use_Count[i] + " 개 " + Mathf.Round(((float)(m_nDictionary_Gift_Item_Use_Probability[i] / (float)10000) * 100)) + " %\n";
+            }
+            for (int i = 0; i < m_nDictionary_Gift_Item_Etc_Code.Count; i++)
+            {
+                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Etc[m_nDictionary_Gift_Item_Etc_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Etc_Count[i] + " 개 " + Mathf.Round(((float)(m_nDictionary_Gift_Item_Etc_Probability[i] / (float)10000) * 100)) + " %\n";
+            }
+
+            return strtoken;
+        }
+        // 3. 소비아이템(기프트(랜덤 지급형 _ B 타입))
+        else if (m_eItemUseGiftType == E_ITEM_USE_GIFT_TYPE.RANDOMBOX_DEPENDENTTRIAL)
+        {
+            strtoken += "[사용 시 아래 아이템을 획득합니다.]\n";
+            strtoken += "[획득할 수 있는 아이템 개수: " + m_nRandomBox_PickCount_Min.ToString() + " ~ " + m_nRandomBox_PickCount_Max.ToString() + " 개.]\n";
+            strtoken += "[아이템 중복 획득이 불가능합니다.]\n";
+            for (int i = 0; i < m_nDictionary_Gift_Item_Equip_Code.Count; i++)
+            {
+                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Equip[m_nDictionary_Gift_Item_Equip_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Equip_Count[i] + " 개 " + Mathf.Round(((float)(m_nDictionary_Gift_Item_Equip_Probability[i] / (float)10000) * 100)) + " %\n";
+            }
+            for (int i = 0; i < m_nDictionary_Gift_Item_Use_Code.Count; i++)
+            {
+                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Use[m_nDictionary_Gift_Item_Use_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Use_Count[i] + " 개 " + Mathf.Round(((float)(m_nDictionary_Gift_Item_Use_Probability[i] / (float)10000) * 100)) + " %\n";
+            }
+            for (int i = 0; i < m_nDictionary_Gift_Item_Etc_Code.Count; i++)
+            {
+                strtoken += ItemManager.instance.m_Dictionary_MonsterDrop_Etc[m_nDictionary_Gift_Item_Etc_Code[i]].m_sItemName + " " + m_nDictionary_Gift_Item_Etc_Count[i] + " 개 " + Mathf.Round(((float)(m_nDictionary_Gift_Item_Etc_Probability[i] / (float)10000) * 100)) + " %\n";
+            }
+
+            return strtoken;
+        }
+        else
+            return "";
     }
 }
