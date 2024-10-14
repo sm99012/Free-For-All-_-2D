6879e385b44eee8a6a98b42364fe1e983cd1f7e0
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-14 15:25:14 +0900

    Update Quest_ROLL.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Quest/Quest_ROLL.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Quest/Quest_ROLL.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Quest_ROLL : Quest
-{
-    public int m_nCount_Max;
-    public int m_nCount_Current;
+//
+// ※ 구르기 퀘스트 클래스
+//    해당 퀘스트는 구르기 행위와 연관되어 있다.
+//
 
+public class Quest_ROLL : Quest // 기반이 되는 Quest 클래스 상속
+{
+    // 구르기 관련 변수
+    public int m_nCount_Max;     // 구르기 필요 수량
+    public int m_nCount_Current; // 구르기 현재 수량
+
+    // 구르기 퀘스트를 생성하는 생성자
     public Quest_ROLL(string questtitle, int questcode, E_QUEST_LEVEL ql, int givenpccode, int clearnpccode, int questloadmapcode)
     {
         m_sQuest_Title = questtitle;
@@ -20,21 +27,20 @@ public class Quest_ROLL : Quest
         InitialSet();
     }
 
-    // Quest 조건에 MaxCount, CurrentCount 추가.
-    public void SetCondition(int max, int c = 0)
+    // 구르기 퀘스트 설정 함수
+    public void SetCondition(int max, int c = 0) // max : 구르기 필요 수량, c : 구르기 현재 수량
     {
         m_nCount_Max = max;
         m_nCount_Current = c;
     }
 
-    // 구르기 횟수 Check
+    // 구르기 퀘스트 갱신 함수. 퀘스트 완료 조건 달성 여부(플레이어가 현재 해당 퀘스트를 완료할 조건을 충족시켰는지?) 판단 함수
     public bool Check_ROLL()
     {
-        //Debug.Log("Roll");
-        if (m_nCount_Max > m_nCount_Current)
+        if (m_nCount_Max > m_nCount_Current) // 구르기 필요 수량 > 구르기 현재 수량
             m_nCount_Current += 1;
 
-        if (m_nCount_Max == m_nCount_Current)
+        if (m_nCount_Max == m_nCount_Current) // 구르기 필요 수량 == 구르기 현재 수량
         {
             m_bCondition = true;
         }
