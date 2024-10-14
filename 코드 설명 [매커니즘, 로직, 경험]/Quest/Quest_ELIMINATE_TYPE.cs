1775bf5ead604b0fb8b14eb0b23844fd9e43732d
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-14 15:36:23 +0900

    Update Quest_ELIMINATE_TYPE.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Quest/Quest_ELIMINATE_TYPE.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Quest/Quest_ELIMINATE_TYPE.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Quest_ELIMINATE_TYPE : Quest
-{
-    public E_MONSTER_KIND m_eMonsterType;
-    public int m_nCount_Max;
-    public int m_nCount_Current;
+//
+// ※ 특정 몬스터 타입 제거(토벌 + 놓아주기) 퀘스트 클래스
+//    해당 퀘스트는 특정 몬스터 타입 제거(토벌 + 놓아주기)(사냥) 행위와 연관되어 있다. 몬스터 타입이란 평판 데이터(인간, 동물, 슬라임, 스켈레톤, 앤트, 마족, 용족, 어둠)와 관련이 있다.
+//
 
+public class Quest_ELIMINATE_TYPE : Quest // 기반이 되는 Quest 클래스 상속
+{
+    // 특정 몬스터 타입 관련 변수
+    public E_MONSTER_KIND m_eMonsterType; // 특정 몬스터 타입
+    public int m_nCount_Max;              // 특정 몬스터 타입 제거(토벌 + 놓아주기) 필요 수량
+    public int m_nCount_Current;          // 특정 몬스터 타입 제거(토벌 + 놓아주기) 현재 수량
+
+    // 특정 몬스터 타입 제거(토벌 + 놓아주기) 퀘스트를 생성하는 생성자
     public Quest_ELIMINATE_TYPE(string questtitle, int questcode, E_QUEST_LEVEL ql, int givenpccode, int clearnpccode, int questloadmapcode)
     {
         m_sQuest_Title = questtitle;
@@ -21,26 +28,28 @@ public class Quest_ELIMINATE_TYPE : Quest
         InitialSet();
     }
 
-    // Quest 조건에 Monster, MaxCount, CurrentCount 추가.
-    public void SetCondition(E_MONSTER_KIND mk, int max, int c = 0)
+    // 특정 몬스터 타입 제거(토벌 + 놓아주기) 퀘스트 설정 함수
+    public void SetCondition(E_MONSTER_KIND mk, int max, int c = 0) // mk : 몬스터 타입, max : 특정 몬스터 타입 제거(토벌 + 놓아주기) 필요 수량, c : 특정 몬스터 타입 제거(토벌 + 놓아주기) 현재 수량
     {
         m_eMonsterType = mk;
         m_nCount_Max = max;
         m_nCount_Current = c;
     }
 
-    public bool Check_ELIMINATE_TYPE(E_MONSTER_KIND mk)
+    // 특정 몬스터 타입 제거(토벌 + 놓아주기) 퀘스트 갱신 함수
+    // return true : 플레이어가 제거(토벌 + 놓아주기)한 몬스터 타입이 해당 특정 몬스터 타입 제거(토벌 + 놓아주기) 퀘스트와 관련 있음 / return false : 플레이어가 제거(토벌 + 놓아주기)한 몬스터 타입이 해당 특정 몬스터 타입 제거(토벌 + 놓아주기) 퀘스트와 관련 없음
+    public bool Check_ELIMINATE_TYPE(E_MONSTER_KIND mk) // mk : 플레이어가 제거(토벌 + 놓아주기)한 몬스터 타입
     {
         bool m_bReturn = false;
-        if (m_eMonsterType == mk)
+        if (m_eMonsterType == mk) // 특정 몬스터 타입 == 플레이어가 제거(토벌 + 놓아주기)한 몬스터 타입
         {
-            if (m_nCount_Max > m_nCount_Current)
+            if (m_nCount_Max > m_nCount_Current) // 특정 몬스터 타입 제거(토벌 + 놓아주기) 필요 수량 > 특정 몬스터 타입 제거(토벌 + 놓아주기) 현재 수량
                 m_nCount_Current++;
 
             m_bReturn = true;
         }
 
-        Check_Condition();
+        Check_Condition(); // 퀘스트 완료 조건 달성 여부(플레이어가 현재 해당 퀘스트를 완료할 조건을 충족시켰는지?) 판단 함수
 
         if (m_bReturn == true)
             return true;
@@ -48,13 +57,14 @@ public class Quest_ELIMINATE_TYPE : Quest
             return false;
     }
 
+    // 퀘스트 완료 조건 달성 여부(플레이어가 현재 해당 퀘스트를 완료할 조건을 충족시켰는지?) 판단 함수
     void Check_Condition()
     {
-        if (m_nCount_Max == m_nCount_Current)
+        if (m_nCount_Max == m_nCount_Current) // 특정 몬스터 타입 제거(토벌 + 놓아주기) 필요 수량 == 특정 몬스터 타입 제거(토벌 + 놓아주기) 현재 수량 : 퀘스트 완료 조건 충족
         {
             m_bCondition = true;
         }
-        else if (m_nCount_Max > m_nCount_Current)
+        else if (m_nCount_Max > m_nCount_Current) // 특정 몬스터 타입 제거(토벌 + 놓아주기) 필요 수량 > 특정 몬스터 타입 제거(토벌 + 놓아주기) 현재 수량 : 퀘스트 완료 조건 미달
         {
             m_bCondition = false;
         }
