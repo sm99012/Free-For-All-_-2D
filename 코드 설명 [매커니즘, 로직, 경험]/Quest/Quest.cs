5a114beeec43136d05c6d036dbe59513c434df58
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-14 10:58:19 +0900

    Update Quest.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Quest/Quest.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Quest/Quest.cs
@@ -2,63 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum E_QUEST_LEVEL { S1, S2, S3, S4, S5, S6, S7, S8, S9, S10 } // 퀘스트 난이도
-public enum E_QUEST_REPEAT { ONCE, FINITE, INFINITE } // 퀘스트 반복 여부 { 단 한번만 수행, 무제한 수행, 제한 수행 }
-public enum E_QUEST_TYPE { NULL, MOVE, ATTACK, ATTACK1_1, ATTACK1_2, ATTACK1_3, ATTACKED, GOAWAY_MONSTER, GOAWAY_TYPE, KILL_MONSTER, KILL_TYPE, COLLECT, CONVERSATION, ROLL, ELIMINATE_MONSTER, ELIMINATE_TYPE }
-// 퀘스트 타입 개발 현황(퀘스트 코드)
-// KILL_MONSTER(특정 몬스터 토벌) : 0 ~ 999(퀘스트 코드)
-// KILL_TYPE(특정 타입의 몬스터 토벌) : 1000 ~ 1999(퀘스트 코드)
-// GOAWAY_MONSTER(특정 몬스터 놓아주기) : 2000 ~ 2999(퀘스트 코드)
-// GOAWAY_TYPE(특정 타입의 몬스터 놓아주기) : 3000 ~ 3999(퀘스트 코드)
-// COLLECT(수집) : 4000 ~ 4999(퀘스트 코드)
-// CONVERSATION(대화) : 5000 ~ 5999(퀘스트 코드)
-// ROLL(구르기) : 6000 ~ 6999(퀘스트 코드)
-// ELIMINATE_MONSTER(특정 몬스터 제거) : 7000 ~ 7999(퀘스트 코드)
-// ELIMINATE_TYPE(특정 타입의 몬스터 제거) : 8000 ~ 8999(퀘스트 코드)
+//
+// ※ 기반이 되는 Quest 클래스를 구현한 후
+//    Quest_KILL_MONSTER(특정 몬스터 토벌), Quest_KILL_TYPE(특정 몬스터 타입 토벌), Quest_GOAWAY_MONSTER(특정 몬스터 놓아주기), Quest_GOAWAY_TYPE(특정 몬스터 타입 놓아주기), 
+//    Quest_COLLECT(수집), Quest_CONVERSATION(대화), Quest_ROLL(구르기), Quest_ELIMINATE_MONSER(특정 몬스터 제거(토벌 + 놓아주기)), Quest_ELIMINATE_TYPE(특정 몬스터 타입 제거(토벌 + 놓아주기))
+//    클래스를 상속으로 구현했다.
+//
 
-// 가장 공들여 설계한 파트가 퀘스트 파트이다.
-// 상속과 가상함수(오버라이딩)를 이용해 다양한 퀘스트를 구현했다. 그러나 여전히 낭비되는 메모리가 존재한다. 인터페이스(추상 클래스)를 사용해 본다면 어떨까 한다.
+// 퀘스트 타입 : { NULL, 이동, 공격, 기본 공격1, 기본 공격2, 기본 공격3, 피격, 특정 몬스터 토벌, 특정 몬스터 타입 토벌, 특정 몬스터 놓아주기, 특정 몬스터 타입 놓아주기, 수집, 대화, 구르기, 특정 몬스터 제거(토벌 + 놓아주기), 특정 몬스터 타입 제거(토벌 + 놓아주기)}
+public enum E_QUEST_TYPE { NULL, MOVE, ATTACK, ATTACK1_1, ATTACK1_2, ATTACK1_3, ATTACKED, KILL_MONSTER, KILL_TYPE, GOAWAY_MONSTER, GOAWAY_TYPE, COLLECT, CONVERSATION, ROLL, ELIMINATE_MONSTER, ELIMINATE_TYPE }
+// 퀘스트 등급(난이도) : { S1, S2, S3, S4, S5, S6, S7, S8, S0, S10 }
+public enum E_QUEST_LEVEL { S1, S2, S3, S4, S5, S6, S7, S8, S9, S10 }
+// 퀘스트 반복 여부 : { 단 한번 수행, 무제한 수행, 제한 수행 }
+public enum E_QUEST_REPEAT { ONCE, FINITE, INFINITE }
+
+//
+// ※ Quest 클래스는 가장 공들여 설계한 클래스 중 하나이다. 상속과 가상함수(오버라이딩)을 이용해 다양한 퀘스트를 구현했다.
+//    인터페이스를 이용한 효율적인 메모리 관리법을 생각하고 있다.
+//
+
 public class Quest : MonoBehaviour
 {
-    public string m_sQuest_Title; // 퀘스트 제목
+    public string m_sQuest_Title;       // 퀘스트 제목
     
-    public E_QUEST_LEVEL m_eQuestLevel; // 퀘스트 난이도
+    public int m_nQuest_Code;           // 퀘스트 고유코드
+                                        // ※ KILL_MONSTER(특정 몬스터 토벌) 고유코드 : 0 ~ 999
+                                        //    KILL_TYPE(특정 몬스터 타입 토벌) 고유코드 : 1000 ~ 1999
+                                        //    GOAWAY_MONSTER(특정 몬스터 놓아주기) 고유코드 : 2000 ~ 2999
+                                        //    GOAWAY_TYPE(특정 몬스터 타입 놓아주기) 고유코드 : 3000 ~ 3999
+                                        //    COLLECT(수집) 고유코드 : 4000 ~ 4999
+                                        //    CONVERSATION(대화) 고유코드 : 5000 ~ 5999
+                                        //    ROLL(구르기) 고유코드 : 6000 ~ 6999
+                                        //    ELIMINATE_MONSTER(특정 몬스터 제거) 고유코드 : 7000 ~ 7999
+                                        //    ELIMINATE_TYPE(특정 몬스터 타입 제거) 고유코드 : 8000 ~ 8999
+                                      
+    public int m_nQuestOrder;           // 퀘스트 진행 및 퀘스트 완료 순서(해당 순서에 따라 퀘스트GUI가 업데이트 된다.)
+    public int m_nQuest_Loadmap_Code;   // 퀘스트 로드맵 순서(추천 퀘스트 출력 순서)
+
     public E_QUEST_TYPE m_eQuestType;   // 퀘스트 타입
-    
+    public E_QUEST_LEVEL m_eQuestLevel; // 퀘스트 등급(난이도)
+
     public int m_nNPC;       // 퀘스트 발행 NPC
     public int m_nNPC_Clear; // 퀘스트 완료 NPC
-    
-    public int m_nQuest_Code;         // 퀘스트 코드
-    public int m_nQuestOrder; // 퀘스트 진행 및 퀘스트 완료 순서(순서에 따라 퀘스트GUI가 업데이트 된다.)
-    public int m_nQuest_Loadmap_Code; // 퀘스트 로드맵 순서(추천 퀘스트 로드맵 출력 순서)
-    
-    public List<string> m_sl_QuestDescription_Context; // 퀘스트 발행 시 대화 스크립트(리스트)
-    public List<string> m_sl_QuestDescription_Simple;  // 퀘스트의 간단한 정보(리스트)
-    public List<string> m_sl_QuestOk_Context;          // 퀘스트 수락 시 대화 스크립트(리스트)
-    public List<string> m_sl_QuestNo_Context;          // 퀘스트 거절 시 대화 스크립트(리스트)
-    public List<string> m_sl_QuestProgress_Context;    // 퀘스트 진행중일 시 대화 스크립트(리스트). 퀘스트 완료 조건을 충족하지 않은 경우
-    public List<string> m_sl_QuestClear_Context;       // 퀘스트 완료 시 대화 스크립트(리스트)
 
-    // 플레이어가 퀘스트를 진행중인지?
-    public bool m_bProcess;
-    // 플레이어가 퀘스트 클리어를 위한 조건을 모두 달성했는지?
-    public bool m_bCondition;
-    // 플레이어가 퀘스트를 클리어 했는지?
-    public bool m_bClear;
+    // 퀘스트 대화 스크립트 관련 변수
+    public List<string> m_sl_QuestDescription_Context; // 퀘스트 발행 시 대화 스크립트
+    public List<string> m_sl_QuestDescription_Simple;  // 퀘스트 간단 설명
+    public List<string> m_sl_QuestOk_Context;          // 퀘스트 수락 시 대화 스크립트
+    public List<string> m_sl_QuestNo_Context;          // 퀘스트 거절 시 대화 스크립트
+    public List<string> m_sl_QuestProgress_Context;    // 퀘스트 진행중일 시 대화 스크립트(퀘스트 완료 조건을 충족시키지 않은 경우 출력되는 대화 스크립트)
+    public List<string> m_sl_QuestClear_Context;       // 퀘스트 완료 시 대화 스크립트
 
-    // 연계 퀘스트 사전 조건 체크
-    // 사전 필수 퀘스트 리스트
-    public List<Quest> m_ql_Quest_Necessity_Clear;
-    // 해당 퀘스트 클리어 기록이 존재하면 퀘스트X
-    public List<Quest> m_ql_Quest_Necessity_NonClear;
-    // 해당 퀘스트를 진행중이면 퀘스트O
-    public List<Quest> m_ql_Quest_Necessity_Process;
-    // 해당 퀘스트를 진행중이면 퀘스트X
-    public List<Quest> m_ql_Quest_Necessity_NonProcess;
-    public STATUS m_sStatus_Necessity_Up;
-    public STATUS m_sStatus_Necessity_Down;
-    public SOC m_sSoc_Necessity_Up;
-    public SOC m_sSoc_Necessity_Down;
+    // 퀘스트 진행 상태 관련 변수
+    public bool m_bProcess;   // 퀘스트 진행 여부(플레이어가 현재 해당 퀘스트를 진행 중인지?)
+    public bool m_bCondition; // 퀘스트 완료 조건 달성 여부(플레이어가 현재 해당 퀘스트를 완료할 조건을 충족시켰는지?)
+    public bool m_bClear;     // 퀘스트 완료 여부(플레이어가 현재 해당 퀘스트를 완료 했는지?)
+
+    // 퀘스트 진행 사전 조건 관련 변수
+    // 퀘스트 진행 사전 조건 - 스탯(능력치) 상한ㆍ하한
+    public STATUS m_sStatus_Necessity_Up;   // 스탯(능력치) 상한(플레이어의 스탯(능력치) 합계가 퀘스트 진행 사전 조건(해당 조건)을 초과한 경우 제한)
+    public STATUS m_sStatus_Necessity_Down; // 스탯(능력치) 하한(플레이어의 스탯(능력치) 합계가 퀘스트 진행 사전 조건(해당 조건)에 미달한 경우 제한)
+    // 퀘스트 진행 사전 조건 - 스탯(평판) 상한ㆍ하한
+    public SOC m_sSoc_Necessity_Up;         // 스탯(평판) 상한(플레이어의 스탯(평판) 합계가 퀘스트 진행 사전 조건(해당 조건)을 초과한 경우 제한)
+    public SOC m_sSoc_Necessity_Down;       // 스탯(평판) 하한(플레이어의 스탯(평판) 합계가 퀘스트 진행 사전 조건(해당 조건)에 미달한 경우 제한)
+    // 퀘스트 진행 사전 조건 - 연계 퀘스트
+    public List<Quest> m_ql_Quest_Necessity_Clear;      // 사전 조건 - 완료 퀘스트(해당 리스트에 포함된 퀘스트의 완료 여부 판단)
+    public List<Quest> m_ql_Quest_Necessity_NonClear;   // 사전 조건 - 미완료 퀘스트(해당 리스트에 포함된 퀘스트의 미완료 여부 판단)
+    public List<Quest> m_ql_Quest_Necessity_Process;    // 사전 조건 - 진행 퀘스트(해당 리스트에 포함된 퀘스트의 진행 여부 판단)
+    public List<Quest> m_ql_Quest_Necessity_NonProcess; // 사전 조건 - 미진행 퀘스트(해당 리스트에 포함된 퀘스트의 미진행 여부 판단)
 
     // 퀘스트 클리어 보상
     public List<Item_Equip> m_lRewardList_Item_Equip;
