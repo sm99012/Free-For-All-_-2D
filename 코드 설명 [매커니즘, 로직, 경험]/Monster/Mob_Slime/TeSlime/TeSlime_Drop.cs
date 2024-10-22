d8e1aab4bf6aa877711619096f50d0f35fd9f1de
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-22 21:03:30 +0900

    Update TeSlime_Drop.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/TeSlime/TeSlime_Drop.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/TeSlime/TeSlime_Drop.cs
@@ -4,16 +4,5 @@ using UnityEngine;
 
 public class TeSlime_Drop : Monster_Drop
 {
-    override public void Start()
-    {
-        InitialSet();
-    }
 
-    override public void InitialSet()
-    {
-        EssentialSet(); ;
-
-        m_vItempos = new Vector3(0, 0, 0);
-        m_vItemoffset = new Vector3(0.001f, 0, 0);
-    }
 }
