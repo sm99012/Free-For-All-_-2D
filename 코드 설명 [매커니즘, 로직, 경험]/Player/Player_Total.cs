8b668307f92dabcdc3a677113e0efc71fa96ed1e
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-23 00:34:07 +0900

    Update Player_Total.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Player/Player_Total.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Player/Player_Total.cs
@@ -47,7 +47,7 @@ public class Player_Total : MonoBehaviour
     public int vInput;      // 수직이동 값
     public int m_nPosValue; // 플레이어가 바라보는 방향(2D 탑다운 게임이지만 플레이어는 우측과 좌측만 바라보고 게임을 진행한다.)
     
-    // 플레이어가 착용한 무기 분류에 따라 상이한 공격 패턴(공격범위, 공격력 계수, 공격 타이밍)을 가진다.
+    // 플레이어가 착용한 무기 분류에 따라 상이한 공격 패턴(공격범위, 데미지 계수, 공격 타이밍)을 가진다.
     // Sword(검) 공격범위
     GameObject m_gAttack_Area_Sword;
     GameObject m_gAttack1_1_Area_Sword;
@@ -330,51 +330,51 @@ public class Player_Total : MonoBehaviour
     }
 
     // 무기 분류별 기본 공격 판정 관련 함수
-    // 기본 공격 애니메이션의 특정 프레임에서 호출된다. 기본 공격의 종류별로 공격범위, 공격력 계수, 공격 타이밍이 다르다. 
+    // 기본 공격 애니메이션의 특정 프레임에서 호출된다. 기본 공격의 종류별로 공격범위, 데미지 계수, 공격 타이밍이 다르다. 
     // Sword(검) 기본 공격 판정
     public void AttackCheck1_1_Sword()
     {
-        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.SWORD, 1, 1f); // 기본 공격1 판정. 공격력 계수 : 100%
+        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.SWORD, 1, 1f); // 기본 공격1 판정. 데미지 계수 : 100%
     }
     public void AttackCheck1_2_Sword()
     {
-        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.SWORD, 2, 2f); // 기본 공격2 판정. 공격력 계수 : 200%
+        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.SWORD, 2, 2f); // 기본 공격2 판정. 데미지 계수 : 200%
     }
     public void AttackCheck1_3_Sword()
     {
-        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.SWORD, 3, 4f); // 기본 공격3 판정. 공격력 계수 : 400%
+        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.SWORD, 3, 4f); // 기본 공격3 판정. 데미지 계수 : 400%
     }
     // Axe(도끼) 기본 공격 판정
     public void AttackCheck1_1_Axe()
     {
-        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.AXE, 1, 1.5f); // 기본 공격1 판정. 공격력 계수 : 150%
+        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.AXE, 1, 1.5f); // 기본 공격1 판정. 데미지 계수 : 150%
     }
     public void AttackCheck1_2_Axe()
     {
-        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.AXE, 2, 2f); // 기본 공격2 판정. 공격력 계수 : 200%
+        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.AXE, 2, 2f); // 기본 공격2 판정. 데미지 계수 : 200%
     }
     public void AttackCheck1_3_Axe()
     {
-        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.AXE, 3, 6f); // 기본 공격3 판정. 공격력 계수 : 600%
+        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.AXE, 3, 6f); // 기본 공격3 판정. 데미지 계수 : 600%
     }
     // Knife(단검) 기본 공격 판정
     public void AttackCheck1_1_Knife()
     {
-        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.KNIFE, 1, 1f); // 기본 공격1 판정. 공격력 계수 : 100%
+        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.KNIFE, 1, 1f); // 기본 공격1 판정. 데미지 계수 : 100%
     }
     public void AttackCheck1_2_Knife()
     {
-        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.KNIFE, 2, 1.5f); // 기본 공격2 판정. 공격력 계수 : 150%
+        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.KNIFE, 2, 1.5f); // 기본 공격2 판정. 데미지 계수 : 150%
     }
     public void AttackCheck1_3_Knife()
     {
-        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.KNIFE, 3, 2.5f); // 기본 공격3 판정. 공격력 계수 : 250%
+        AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE.KNIFE, 3, 2.5f); // 기본 공격3 판정. 데미지 계수 : 250%
     }
 
     // 기본 공격 판정 함수
-    // 오버랩을 이용해 공격 범위내의 모든 오브젝트(몬스터, 파괴 가능한 오브젝트)에 특정 공격력 계수를 적용한 데미지를 가한다.
+    // 오버랩을 이용해 공격 범위내의 모든 오브젝트(몬스터, 파괴 가능한 오브젝트)에 특정 데미지 계수를 적용한 데미지를 가한다.
     // Physics2D.OverlapBoxAll(Vector2 point, Vector2 size, float angle, int layerMask) // point : 오버랩 지점, size : 오버랩 크기, angle : 각도, layerMask : 오버랩을 적용할 레이어 // return Collider2D[]
-    void AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE mt, int attacknumber, float percent) // mt : 무기 분류, attacknumber : 기본 공격 단계(1, 2, 3), percent : 공격력 계수
+    void AttackCheck(E_ITEM_EQUIP_MAINWEAPON_TYPE mt, int attacknumber, float percent) // mt : 무기 분류, attacknumber : 기본 공격 단계(1, 2, 3), percent : 데미지 계수
     {
         if (mt == E_ITEM_EQUIP_MAINWEAPON_TYPE.SWORD) // 무기 분류 : Sword(검)
         {
