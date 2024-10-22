f22f3e30192edb6ba1a9153938ed6af5a3f103b9
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-22 21:02:36 +0900

    Update Slime3_Drop.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime3/Slime3_Drop.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime3/Slime3_Drop.cs
@@ -4,25 +4,5 @@ using UnityEngine;
 
 public class Slime3_Drop : Monster_Drop
 {
-    override public void Start()
-    {
-        InitialSet();
-    }
-
-    override public void InitialSet()
-    {
-        EssentialSet();
-
-        m_vItempos = new Vector3(0, 0, 0);
-        m_vItemoffset = new Vector3(0.001f, 0, 0);
-
-        //m_List_DropItem_Etc.Add(ItemManager.instance.m_Dictionary_MonsterDrop_Etc[0001]);
-        //m_List_DropPercent_Etc.Add(2000);
-        //m_List_DropItem_Etc.Add(ItemManager.instance.m_Dictionary_MonsterDrop_Etc[0011]);
-        //m_List_DropPercent_Etc.Add(500);
-
-        //m_List_DropItem_Gold_Min.Add(1);
-        //m_List_DropItem_Gold_Max.Add(2);
-        //m_List_DropPercent_Gold.Add(1500);
-    }
+    
 }
