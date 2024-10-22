ed467d337d1d6e629d7a9c0096841ac81808ab42
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-22 23:54:40 +0900

    Update Monster_Status.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Monster_Status.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Monster_Status.cs
@@ -17,7 +17,7 @@ public class Monster_Status : MonoBehaviour
 { 
     public string m_sMonsterName;          // 몬스터 이름
     
-    public int m_nMonsterCode;             //몬스터 고유코드
+    public int m_nMonsterCode;             // 몬스터 고유코드
     
     public E_OBJECT_STATE m_eObject_State; // 오브젝트 타입
     public E_MONSTER_KIND m_eMonster_Kind; // 몬스터 타입
@@ -34,16 +34,18 @@ public class Monster_Status : MonoBehaviour
     public SOC m_sSoc_null;                // null값을 가지는 평판. 놓아주기 실패 시 사용된다.
     public SOC m_sSoc_Goaway;              // 몬스터 놓아주기 시 획득 가능한 스탯(평판)
 
-    public bool m_bDestroy = true;         // 몬스터 현재체력이 0이 되어도 죽지않는 특수 조건 
+    public bool m_bDestroy = true;         // 몬스터 현재체력이 0 이 되어도 죽지않는 특수 조건 
     public bool m_bDeath = false;          // 몬스터 사망 여부
 
-    // 데미지 출력 오프셋
-    protected Vector3 m_vDamageOffSet;
+    protected Vector3 m_vDamageOffSet;     // 몬스터 별 데미지 출력 오프셋
+    int m_nDamage;                         // 몬스터 피격 데미지(출력 전용. 소숫점 반올림)
+    float m_fDamage;                       // 몬스터 피격 데미지(소숫점 계산 가능)
 
     void Start()
     {
-        m_sStatus = new STATUS();
         m_eObject_State = E_OBJECT_STATE.MONSTER;
+        
+        m_sStatus = new STATUS();
 
         InitialSet_SOC();
         InitialSet_STATUS();
@@ -51,55 +53,47 @@ public class Monster_Status : MonoBehaviour
         m_vDamageOffSet = new Vector3(0, 0.05f, 0);
     }
 
-    
-    void Update()
-    {
-        
-    }
-
-    virtual public void Respone()
-    {
-        m_sStatus.SetSTATUS(m_sStatus_Origin);
-    }
-
+    // 몬스터 스탯(평판) 초기 설정
     virtual public void InitialSet_SOC()
     {
         m_sSoc_null = new SOC();
-        m_sSoc_Goaway = new SOC();
-        m_sSoc_Death = new SOC();
+        m_sSoc_Goaway = new SOC(); // 몬스터 놓아주기 시 획득 가능한 스탯(평판)
+        m_sSoc_Death = new SOC();  // 몬스터 토벌 시 획득 가능한 스탯(평판)
     }
-
+    // 몬스터 스탯(능력치) 초기 설정
     virtual public void InitialSet_STATUS()
     {
         m_sStatus = new STATUS();
-        m_sStatus_Goaway = new STATUS();
-        m_sStatus_Death = new STATUS();
+        m_sStatus_Goaway = new STATUS(); // 몬스터 놓아주기 시 획득 가능한 스탯(능력치)
+        m_sStatus_Death = new STATUS();  // 몬스터 토벌 시 획득 가능한 스탯(능력치)
 
         m_sStatus_Origin.SetSTATUS(m_sStatus);
     }
 
-    int m_nDamage;
-    float m_fDamage;
-    virtual public bool Attacked(int dm, float dmrate)
+    // 몬스터 피격 시 스탯(능력치) 변동 함수
+    // return true : 몬스터 사망 / return false : 몬스터 생존
+    virtual public bool Attacked(int dm, float dmrate) // dm : 피격 데미지, dmrate : 피격 데미지 계수
     {
+        // 오브젝트 타입 == 몬스터 : 피격 데미지 계산
         if (m_eObject_State == E_OBJECT_STATE.MONSTER)
         {
-            // Defence_Physical
+            // 몬스터 피격 시 피격 데미지 계산
+            // 데미지, 방어력 계산 공식 : 데미지 * (물리방어력 / (10(방어력 계수) + 물리방어력)). ※ 데미지, 방어력 계산 공식은 추후 변경될 수 있다.(단순 데미지 - 방어력 -> 방어력 계수 적용)
             m_fDamage = (int)((float)dm * ((float)m_sStatus.GetSTATUS_Defence_Physical() / ((float)(10) + (float)m_sStatus.GetSTATUS_Defence_Physical())));
-            m_nDamage = dm - (int)Mathf.Round(m_fDamage);
-            //m_nDamage = dm - m_sStatus.GetSTATUS_Defence_Physical();
+            m_nDamage = dm - (int)Mathf.Round(m_fDamage); // 데미지의 소숫점 제거(반올림)
             if (m_nDamage <= 0)
-                m_nDamage = -1;
+                m_nDamage = -1; // 최소 피격 데미지 = -1
             else
-                m_nDamage = (int)(-m_nDamage * dmrate);
-            m_bDeath = Operator_HP(m_nDamage);
+                m_nDamage = (int)(-m_nDamage * dmrate); // 피격 데미지 계수 적용(플레이어 착용 무기 분류별 데미지 계수 적용)
+            m_bDeath = Operator_HP(m_nDamage); //
         }
+        // 오브젝트 타입 == 오브젝트(장애물) : 모든 피격 데미지 1 고정
         else if (m_eObject_State == E_OBJECT_STATE.HURDLE)
         {
             m_nDamage = -1;
             m_bDeath = Operator_HP(m_nDamage);
         }
-        //GUIManager_Total.Instance.UpdateLog(m_sMonsterName + "이(가) " + Damage.ToString() + " 의 데미지를 입었다.");
+        // 몬스터 피격 데미지 출력
         GameObject obj = Resources.Load("Prefab/GUI/TextMesh_Damage") as GameObject;
         GameObject copyobj = Instantiate(obj);
         copyobj.GetComponent<TextMesh_Damage>().InitialSet(this.transform.position + m_vDamageOffSet, -m_nDamage);
@@ -110,9 +104,11 @@ public class Monster_Status : MonoBehaviour
             return false;
     }
 
-    bool Operator_HP(int dm)
+    // 몬스터 피격 시 스탯(능력치(몬스터 현재체력)) 계산 함수
+    // return true : 몬스터 사망 / return false : 몬스터 생존
+    bool Operator_HP(int dm) // dm : 피격 데미지
     {
-        if (m_sStatus.GetSTATUS_HP_Current() + dm <= 0)
+        if (m_sStatus.GetSTATUS_HP_Current() + dm <= 0) // 몬스터 사망
         {
             m_sStatus.SetSTATUS_HP_Current(0);
             if (m_bDestroy == true)
@@ -120,16 +116,22 @@ public class Monster_Status : MonoBehaviour
             else
                 return false;
         }
-        else
+        else // 몬스터 생존
         {
             m_sStatus.P_OperatorSTATUS_HP_Current(dm);
-            //Debug.Log(m_nStatus.GetSTATUS_HP_Current());
             return false;
         }
     }
 
+    // 몬스터 놓아주기 관련 함수
     virtual public void Goaway()
     {
+        
+    }
 
+    // 몬스터 리스폰 함수
+    virtual public void Respone()
+    {
+        m_sStatus.SetSTATUS(m_sStatus_Origin);
     }
 }
