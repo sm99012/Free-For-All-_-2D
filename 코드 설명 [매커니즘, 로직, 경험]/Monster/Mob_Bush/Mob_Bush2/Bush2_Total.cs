d498e718619146f41e625f52a8bd15f8f232f012
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-23 15:46:39 +0900

    Update Bush2_Total.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush2/Bush2_Total.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush2/Bush2_Total.cs
@@ -21,4 +21,43 @@ public class Bush2_Total : Bush1_Total // 기반이 되는 Bush1_Total 클래스
             }
         }
     }
+
+    // 몬스터 이동 함수 - "수풀"은 이동하지 않는다.
+    // override public void Move() { }
+
+    // 몬스터 이동 방향 설정 함수 - "수풀"은 이동 방향 설정을 하지 않는다.
+    // override public void SetDir() { }
+    
+    // 몬스터 추격 함수 - "수풀"은 추격하지 않는다.
+    // override public void Chase() { }
+
+    // 몬스터 탐지 함수 - "수풀"은 탐지하지 않는다.
+    // override public void Detect() { }
+
+    // 몬스터 공격 함수 - "수풀"은 공격하지 않는다.
+    // override public bool Attack(float attackspeed) { }
+
+    // 몬스터 공격 판정 함수 - "수풀"은 공격하지 않기에 공격 판정이 필요 없다.
+    // override public void Attack_Check() { }
+
+    // 몬스터 접촉 시 오브젝트(플레이어) 피격 판정 함수(몸박뎀 판정) - 부모 클래스인 Bush1_Total의 BodyDamage() 코루틴을 사용한다.
+    // override public void BodyDamage(float percent, float radius, Vector3 offset, float knockbacktime = 0.3F) {ㆍㆍㆍ}
+
+    // 몬스터 피격 함수 - 부모 클래스인 Monster_Total의 Attacked() 코루틴을 사용한다.
+    // override public bool Attacked(int dm,  float dmrate, GameObject gm) // dm : 피격 데미지, dmrate : 피격 데미지 계수, gm : 몬스터 타격 대상(플레이어)
+
+    // 몬스터 사망 함수 + 리스폰 함수 - 부모 클래스인 Monster_Total의 Death() 함수를 사용한다.
+    // virtual public void Death(float time) {ㆍㆍㆍ}
+
+    // 몬스터 놓아주기 판정 함수 - "수풀"은 놓아주기가 불가능하다.
+    // override public SOC Goaway_Check() { }
+
+    // 몬스터 사망 코루틴 - 부모 클래스인 Monster_Total의 ProcessRespone() 코루틴을 사용한다.
+    // virtual public IEnumerator ProcessRespone(float time) {ㆍㆍㆍ}
+
+    // 몬스터 리스폰 함수 - 부모 클래스인 Monster_Total의 Respone() 함수를 사용한다.
+    // virtual public void Respone() {ㆍㆍㆍ}
+
+    // Fadein 효과 연출 함수 - 부모 클래스인 Monster_Total의 Fadein() 함수를 사용한다.
+    // virtual public void Fadein() {ㆍㆍㆍ}
 }
