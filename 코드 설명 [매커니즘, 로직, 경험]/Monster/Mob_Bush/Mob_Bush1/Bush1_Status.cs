cae9596f0a85e382ba9329222bb2a8341bbcecb1
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-23 17:05:01 +0900

    Update Bush1_Status.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush1/Bush1_Status.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush1/Bush1_Status.cs
@@ -36,6 +36,9 @@ public class Bush1_Status : Monster_Status // 기반이 되는 Monster_Status �
 
     // 몬스터 피격 시 스탯(능력치) 변동 함수 - 부모 클래스인 Monster_Status의 Attacked() 함수를 사용한다.
     // virtual public bool Attacked(int dm, float dmrate) {ㆍㆍㆍ}
+
+    // 몬스터 피격 시 스탯(능력치(몬스터 현재체력)) 계산 함수 - 부모 클래스인 Monster_Status의 Operator_HP() 함수를 사용한다.
+    // bool Operator_HP(int dm) {ㆍㆍㆍ}
     
     // 몬스터 놓아주기 관련 함수
     override public void Goaway()
