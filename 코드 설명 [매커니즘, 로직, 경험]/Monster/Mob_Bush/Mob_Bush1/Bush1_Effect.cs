de19f66a36adebc7781a73630562ab932185e7bd
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-23 19:21:06 +0900

    Update Bush1_Effect.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush1/Bush1_Effect.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush1/Bush1_Effect.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Bush1_Effect : Monster_Effect // 기반이 되는 Monster_Effect 클래스 상속
 {
+    // 부모 클래스인 Monster_Effect의 Start() 함수를 사용한다.
+    // void Start() {ㆍㆍㆍ}
+    
     // 몬스터 놓아주기 이펙트 연출 함수 - 부모 클래스인 Monster_Effect의 Effect_Goaway() 함수를 사용한다.
     // virtual public void Effect_Goaway(Vector3 pos) {ㆍㆍㆍ}
     // 몬스터 놓아주기 이펙트 연출 코루틴 - 부모 클래스인 Monster_Effect의 ProcessEffect_Goaway() 코루틴을 사용한다.
