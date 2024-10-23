5863aec85c9b9d74d43688891dc078d07bdde349
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-23 11:13:59 +0900

    Update Bush1_Total.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush1/Bush1_Total.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush1/Bush1_Total.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bush1_Total : Monster_Total
+//
+// ※ "수풀"은 고정형 오브젝트(장애물)로 설계했다.
+//
+
+public class Bush1_Total : Monster_Total // 기반이 되는 Monster_Total 클래스 상속
 {
+    Vector3 m_vSize_HitBody_Offset = new Vector3(0, -0.043f, 0); // 몬스터 접촉 범위 오프셋
+    
     private void Awake()
     {
         m_mm_Move = this.gameObject.GetComponent<Monster_Move>();
@@ -11,38 +17,35 @@ public class Bush1_Total : Monster_Total
         m_md_Drop = this.gameObject.GetComponent<Monster_Drop>();
         m_me_Effect = this.gameObject.GetComponent<Monster_Effect>();
 
-        m_bSetTime = true;
-        m_bRelation = true;
+        m_vSize_HitBody = new Vector2(0.1f, 0.07f);
+        m_vSize_HitBody_Offset = new Vector3(0, -0.043f, 0);
 
-        nLayer1 = 1 << LayerMask.NameToLayer("Player");
+        m_bWait = false; // 다른 오브젝트와 상호작용 가능
+        m_bSetTime = true; // 몬스터 이동 방향 설정 가능
+        m_bRelation = true; // 몬스터 접촉 시 오브젝트(플레이어) 피격 가능(몬스터 몸박뎀 존재)
 
-        m_bWait = false;
+        // 레이어 설정
+        nLayer1 = 1 << LayerMask.NameToLayer("Player"); // 몬스터와 충돌 가능한 오브젝트(플레이어) 레이어
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (m_bPlay == true)
+        if (m_bPlay == true) // 몬스터 동작 가능할 경우
         {
             if (m_bRelation == true && m_bWait == false)
             {
-                BodyDamage();
+                BodyDamage(1, 0, m_vSize_HitBody_Offset); // 몬스터 접촉 시 오브젝트(플레이어) 피격 판정 함수(몸박뎀 판정)
             }
         }
-
-        //AnimationTest();
     }
 
+    // 몬스터 이동 함수 - "수풀"은 이동하지 않는다.
     override public void Move()
     {
 
     }
 
-    public override void Chase()
-    {
-
-    }
-
+    // 몬스터 이동 방향 설정 함수 - "수풀"은 이동 방향 설정을 하지 않는다.
     override public void SetDir()
     {
         if (m_bSetTime == true)
@@ -50,7 +53,44 @@ public class Bush1_Total : Monster_Total
             m_vDir = Vector3.Normalize(new Vector3(0, 0, 0));
         }
     }
+    
+    // 몬스터 추격 함수 - "수풀"은 추격하지 않는다.
+    public override void Chase()
+    {
 
+    }
+
+    // 몬스터 탐지 함수 - "수풀"은 탐지하지 않는다.
+    override public void Detect()
+    {
+
+    }
+
+    // 몬스터 공격 함수 - "수풀"은 공격하지 않는다.
+    // return false : 몬스터 공격 실패(공격속도 영향)
+    override public bool Attack(float attackspeed) // attakcspeed : 몬스터 공격속도
+    {
+        return false;
+    }
+
+    // 몬스터 접촉 시 오브젝트(플레이어) 피격 판정 함수(몸박뎀 판정).
+    // 오버랩을 이용해 범위내의 모든 오브젝트(플레이어)에 특정 데미지 계수를 적용한 데미지를 가한다.
+    public override void BodyDamage(float percent, float radius, Vector3 offset, float knockbacktime = 0.3F)
+    {
+        co2_2 = Physics2D.OverlapBoxAll(this.transform.position + offset, m_vSize_HitBody, 0, nLayer1);
+        if (co2_2.Length > 0)
+        {
+            for (int i = 0; i < co2_2.Length; i++)
+            {
+                if (co2_2[i].gameObject.layer == LayerMask.NameToLayer("Player"))
+                {
+                    m_vKnockBackDir = Vector3.Normalize(co2_2[i].gameObject.transform.position - this.transform.position);
+                    co2_2[i].GetComponent<Player_Total>().Attacked((int)((float)m_ms_Status.m_sStatus.GetSTATUS_Damage_Total() * percent), m_vKnockBackDir, knockbacktime, m_ms_Status.m_sMonsterName);
+                }
+            }
+        }
+    }
+    
     IEnumerator ProcessSetTime()
     {
         m_bSetTime = false;
@@ -103,39 +143,5 @@ public class Bush1_Total : Monster_Total
         }
 
         return m_ms_Status.m_sSoc_null;
-    }
-
-
-    override public void Detect()
-    {
-
-    }
-
-    // 평시 몸박뎀
-    Vector2 m_vSize1 = new Vector2(0.1f, 0.07f);
-    Vector2 m_vOffset = new Vector2(0, -0.043f);
-    Vector2 m_vPos;
-    virtual public void BodyDamage()
-    {
-        m_vPos = new Vector2(this.transform.position.x, this.transform.position.y) + m_vOffset;
-        co2_2 = Physics2D.OverlapBoxAll(m_vPos, m_vSize1, nLayer1);
-        if (co2_2.Length > 0)
-        {
-            for (int i = 0; i < co2_2.Length; i++)
-            {
-                //Debug.Log(co2_2[i].gameObject.name);
-                if (co2_2[i].gameObject.layer == LayerMask.NameToLayer("Player"))
-                {
-                    m_vKnockBackDir = Vector3.Normalize(co2_2[i].gameObject.transform.position - this.transform.position);
-                    co2_2[i].GetComponent<Player_Total>().Attacked((int)((float)m_ms_Status.m_sStatus.GetSTATUS_Damage_Total()), m_vKnockBackDir, 0.3f, m_ms_Status.m_sMonsterName);
-                }
-            }
-        }
-    }
-
-
-    override public bool Attack(float attackspeed)
-    {
-        return false;
     }
 }
