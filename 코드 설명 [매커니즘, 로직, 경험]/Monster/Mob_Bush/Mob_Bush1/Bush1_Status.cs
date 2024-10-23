b72f311f3fd10463533e675e792237785a83794e
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-23 13:10:26 +0900

    Update Bush1_Status.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush1/Bush1_Status.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush1/Bush1_Status.cs
@@ -2,28 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bush1_Status : Monster_Status
+public class Bush1_Status : Monster_Status // 기반이 되는 Monster_Status 클래스 상속
 {
-    // Start is called before the first frame update
     void Start()
     {
+        m_sMonsterName = "수풀";
+        
+        m_nMonsterCode = 8;
+        
         m_eObject_State = E_OBJECT_STATE.HURDLE;
         m_eMonster_Kind = E_MONSTER_KIND.OBJECT;
 
-        InitialSet_SOC();
-        InitialSet_STATUS();
-
-        m_sMonsterName = "수풀";
-        m_nMonsterCode = 8;
+        InitialSet_SOC(); // 몬스터 스탯(평판) 초기 설정
+        InitialSet_STATUS(); // 몬스터 스탯(능력치) 초기 설정
     }
 
+    // 몬스터 스탯(평판) 초기 설정
     override public void InitialSet_SOC()
     {
         m_sSoc_null = new SOC();
         m_sSoc_Goaway = new SOC(0, 0, 0, 0, 0, 0, 0, 0);
         m_sSoc_Death = new SOC(0, 0, 0, 0, 0, 0, 0, 0);
     }
-
+    // 몬스터 스탯(능력치) 초기 설정
     override public void InitialSet_STATUS()
     {
         m_sStatus = new STATUS(1, 0, 0, 5, 5, 0, 0);
@@ -32,8 +33,18 @@ public class Bush1_Status : Monster_Status
         m_sStatus_Death = new STATUS(0, 0, 0);
     }
 
+    // 몬스터 피격 시 스탯(능력치) 변동 함수 - 부모 클래스인 Monster_Status의 Attacked() 함수를 사용한다.
+    // virtual public bool Attacked(int dm, float dmrate) { }
+
+    // 몬스터 피격 시 스탯(능력치(몬스터 현재체력)) 계산 함수 - 부모 클래스인 Monster_Status의 Operator_HP() 함수를 사용한다.
+    //bool Operator_HP(int dm) { }
+    
+    // 몬스터 놓아주기 관련 함수
     override public void Goaway()
     {
         m_bPower = true;
     }
+
+    // 몬스터 리스폰 함수 - 부모 클래스인 Monster_Status의 Respone() 함수를 사용한다.
+    // virtual public void Respone() { }
 }
