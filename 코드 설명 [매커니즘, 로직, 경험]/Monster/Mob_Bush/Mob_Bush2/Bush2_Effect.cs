c0b60e534c65e4fe522e0a846b783ed4eadb9c22
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-23 15:58:33 +0900

    Update Bush2_Effect.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush2/Bush2_Effect.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush2/Bush2_Effect.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bush2_Effect : Bush1_Effect
+public class Bush2_Effect : Bush1_Effect // 기반이 되는 Bush1_Effect 클래스 상속
 {
+    // 몬스터 놓아주기 이펙트 연출 함수 - 부모 클래스인 Monster_Effect의 Effect_Goaway() 함수를 사용한다.
+    // virtual public void Effect_Goaway(Vector3 pos) { }
+    // 몬스터 놓아주기 이펙트 연출 코루틴 - 부모 클래스인 Monster_Effect의 ProcessEffect_Goaway() 코루틴을 사용한다.
+    // virtual protected IEnumerator ProcessEffect_Goaway(Vector3 pos) { }
     
+    // 몬스터 ㆍㆍㆍ 이펙트 연출 함수 - "가시덤불"은 별도의 추가 이펙트를 가지지 않는다.
+    // override public void Effect1(Vector3 pos) { }
+     // 몬스터 ㆍㆍㆍ 이펙트 연출 코루틴 - "가시덤불"은 별도의 추가 이펙트를 가지지 않는다.
+    // override public void Effect1(Vector3 pos, int damage, string attackname) { }
 }
