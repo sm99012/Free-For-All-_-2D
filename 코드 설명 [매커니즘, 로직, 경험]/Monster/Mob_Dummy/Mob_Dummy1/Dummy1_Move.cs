e732df41f5be7495c87f7da82ce175a7431029d4
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-23 19:32:35 +0900

    Update Dummy1_Move.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Dummy/Mob_Dummy1/Dummy1_Move.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Dummy/Mob_Dummy1/Dummy1_Move.cs
@@ -35,7 +35,7 @@ public class Dummy1_Move : Monster_Move // 기반이 되는 Monster_Move 클래�
     // 몬스터 이동 함수 - "훈련용 허수아비"는 이동하지 않는다.
     override public void Move(int speed, Vector3 dir) { }
 
-    // 몬스터 방향 설정 - "훈련용 허수아비"는 이동 방향 설정을 하지 않는다.
+    // 몬스터 방향 설정 함수 - "훈련용 허수아비"는 이동 방향 설정을 하지 않는다.
     override public void SetDir(Vector3 dir) { }
 
     // 몬스터 추격 함수 - "훈련용 허수아비"는 추격하지 않는다.
