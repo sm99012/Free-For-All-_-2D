48551d97adb76c4ac99719116f195d229da068da
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-23 14:36:08 +0900

    Update Bush1_Drop.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush1/Bush1_Drop.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush1/Bush1_Drop.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//
+// ※ 몬스터 제거(토벌 + 놓아주기) 시 플레이어가 획득 가능한 아이템을 관리하는 Monster_Drop 클래스는 MonsterManager 클래스로 통합되었다.
+//
+
 public class Bush1_Drop : Monster_Drop
 {
     
