b403d439de443008e5cfb78baeaefe3fdda58bda
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-23 17:28:30 +0900

    Update Bush2_Move.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush2/Bush2_Move.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush2/Bush2_Move.cs
@@ -31,22 +31,25 @@ public class Bush2_Move : Bush1_Move // 기반이 되는 Bush1_Move 클래스 �
     // void Start() {ㆍㆍㆍ}
 
     // 몬스터 이동 함수 - "가시덤불"은 이동하지 않는다.
-    // override public void Move(int speed, Vector3 dir) { }
+    override public void Move(int speed, Vector3 dir) { }
 
     // 몬스터 방향 설정 - "가시덤불"은 이동 방향 설정을 하지 않는다.
-    // override public void SetDir(Vector3 dir) { }
+    override public void SetDir(Vector3 dir) { }
 
     // 몬스터 추격 함수 - "가시덤불"은 추격하지 않는다.
-    // override public void Chase(int speed, Vector3 dir) { }
+    override public void Chase(int speed, Vector3 dir) { }
     // 몬스터 추격 시간 계산 코루틴 - "가시덤불"은 추격하지 않는다.
-    // override protected IEnumerator ProcessPeaceful() { }
+    override protected IEnumerator ProcessPeaceful() { }
     
-    // 몬스터 공격 함수 - 부모 클래스인 Bush1_Move의 Attack() 함수를 사용한다.
-    // override public bool Attack(float attackspeed) {ㆍㆍㆍ}
+    // 몬스터 공격 함수
+    override public bool Attack(float attackspeed)
+    {
+        return false;
+    }
     // 몬스터 공격속도 계산 코루틴 - "가시덤불"은 공격하지 않는다.
-    // override protected IEnumerator ProcessAttack(float attackspeed) { }
+    override protected IEnumerator ProcessAttack(float attackspeed) { }
     // 몬스터 공격 종료 함수(가상 함수) - "가시덤불"은 공격하지 않는다.
-    // override protected void EndAttack() { }
+    override protected void EndAttack() { }
 
     // 몬스터 피격 함수 - 부모 클래스인 Bush1_Move의 Attacked() 함수를 사용한다.
     // override public void Attacked() {ㆍㆍㆍ}
@@ -61,7 +64,7 @@ public class Bush2_Move : Bush1_Move // 기반이 되는 Bush1_Move 클래스 �
     // virtual public IEnumerator ProcessDeath() {ㆍㆍㆍ}
 
     // 몬스터 놓아주기 함수 - "가시덤불"은 놓아주기가 불가능하다.
-    // virtual public void Goaway() { }
+    virtual public void Goaway() { }
     // 몬스터 놓아주기 시간 계산 코루틴. Fadeout 효과 관련 계산 - "가시덤불"은 놓아주기가 불가능하다.
     // virtual public IEnumerator ProcessGoaway()
 
@@ -75,5 +78,5 @@ public class Bush2_Move : Bush1_Move // 기반이 되는 Bush1_Move 클래스 �
     // virtual public E_MONSTER_MOVE_STATE SetMonsterMoveState(E_MONSTER_MOVE_STATE ms, float attackspeed = 0) {ㆍㆍㆍ}
 
     // 애니메이션 관리(가상 함수) - "가시덤불"은 애니메이션이 존재하지 않는다.
-    // override public void SetAnimationParameters(string str) { }
+    override public void SetAnimationParameters(string str) { }
 }
