401713744e31acb2f5363265b08626730b000098
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-23 14:26:25 +0900

    Update Bush1_Move.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush1/Bush1_Move.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush1/Bush1_Move.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bush1_Move : Monster_Move
+public class Bush1_Move : Monster_Move // 기반이 되는 Monster_Move 클래스 상속
 {
     private void Awake()
     {
@@ -11,62 +11,76 @@ public class Bush1_Move : Monster_Move
         m_tTransform = this.gameObject.GetComponent<Transform>();
         m_rRigdbody = this.gameObject.GetComponent<Rigidbody2D>();
 
+        m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.IDLE);
+        
         m_vRightPos = new Vector3(1, 1, 1);
         m_vLeftPos = new Vector3(-1, 1, 1);
-
-        m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.IDLE);
-
         m_bFix = false;
 
+        m_bAttack = true;
+        m_fPeacefulTime = 10f;
+        
+        m_FadeinAlpa = 0;
         if (m_sSpriteRenderer != null)
         {
             m_fAlpa = m_sSpriteRenderer.color.a;
         }
-        m_FadeinAlpa = 0;
-
-        m_fPeacefulTime = 10f;
-
-        m_bAttack = true;
     }
 
     void Start()
     {
-        Fadein();
+        Fadein(); // Fadein 효과 연출 함수
     }
 
-    override public void Move(int speed, Vector3 dir)
-    {
+    // 몬스터 이동 함수 - "수풀"은 이동하지 않는다.
+    // override public void Move(int speed, Vector3 dir) { }
 
+    // 몬스터 방향 설정 - "수풀"은 이동 방향 설정을 하지 않는다.
+    // override public void SetDir(Vector3 dir) { }
+
+    // 몬스터 추격 함수 - "수풀"은 추격하지 않는다.
+    // override public void Chase(int speed, Vector3 dir) { }
+    // 몬스터 추격 시간 계산 코루틴 - "수풀"은 추격하지 않는다.
+    // override protected IEnumerator ProcessPeaceful() { }
+    
+    // 몬스터 공격 함수
+    override public bool Attack(float attackspeed)
+    { 
+        return false;
     }
+    // override public bool Attack(float attackspeed) { }
+    // 몬스터 공격속도 계산 코루틴 - "수풀"은 공격하지 않는다.
+    // override protected IEnumerator ProcessAttack(float attackspeed) { }
+    // 몬스터 공격 종료 함수(가상 함수) - "수풀"은 공격하지 않는다.
+    // override protected void EndAttack() { }
 
-    override public void Chase(int speed, Vector3 dir)
-    {
-
-    }
-
-    override public void SetDir(Vector3 dir)
-    {
-
-    }
-
+    // 몬스터 피격 함수
     override public void Attacked()
     {
         if (m_eMonsterState == E_MONSTER_MOVE_STATE.IDLE || m_eMonsterState == E_MONSTER_MOVE_STATE.RUN ||
             m_eMonsterState == E_MONSTER_MOVE_STATE.CHASE || m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACKED)
             m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.ATTACKED);
     }
+    // 몬스터 피격 시간 계산 코루틴1 - 부모 클래스인 Monster_Move의 ProcessAttacked1() 코루틴을 사용한다.
+    // virtual protected IEnumerator ProcessAttacked1()
+    // 몬스터 피격 시간 계산 코루틴2 - 부모 클래스인 Monster_Move의 ProcessAttacked2() 코루틴을 사용한다.
+    // virtual protected IEnumerator ProcessAttacked2()
 
-    override public void Goaway()
-    {
-
-    }
-
+    // 몬스터 사망 함수. Fadeout 효과 관련
     override public void Death()
     {
         m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.DEATH);
         StartCoroutine(ProcessDeath());
     }
+    // 몬스터 사망 시간 계산 코루틴. Fadeout 효과 관련 계산 - 부모 클래스인 Monster_Move의 ProcessDeath() 코루틴을 사용한다.
+    // virtual public IEnumerator ProcessDeath() { }
 
+    // 몬스터 놓아주기 함수 - "수풀"은 놓아주기가 불가능하다.
+    // virtual public void Goaway() { }
+    // 몬스터 놓아주기 시간 계산 코루틴. Fadeout 효과 관련 계산 - "수풀"은 놓아주기가 불가능하다.
+    // virtual public IEnumerator ProcessGoaway()
+
+    // 몬스터 리스폰 함수
     override public void Respone()
     {
         m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.IDLE);
@@ -78,13 +92,6 @@ public class Bush1_Move : Monster_Move
         Fadein();
     }
 
-    override public bool Attack(float attackspeed)
-    { 
-        return false;
-    }
-
-    override public E_MONSTER_MOVE_STATE SetMonsterMoveState(E_MONSTER_MOVE_STATE ms, float attackspeed = 0)
-    {
-        return ms;
-    }
+    // 몬스터 동작 FSM 변경 함수(가상 함수) - 부모 클래스인 Monster_Move의 SetMonsterMoveState() 함수를 사용한다.
+    // virtual public E_MONSTER_MOVE_STATE SetMonsterMoveState(E_MONSTER_MOVE_STATE ms, float attackspeed = 0) { }
 }
