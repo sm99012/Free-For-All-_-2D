47f11a0fd191b22da683e4cf09e713a34515d9e8
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-23 15:37:47 +0900

    Update Bush2_Move.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush2/Bush2_Move.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush2/Bush2_Move.cs
@@ -2,30 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bush2_Move : Bush1_Move
+public class Bush2_Move : Bush1_Move // 기반이 되는 Bush1_Move 클래스 상속
 {
     private void Awake()
     {
         m_sSpriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
-        //m_sSpriteRenderer_Shadow = this.gameObject.transform.Find("Bush Shadow").GetComponent<SpriteRenderer>();
+        //m_sSpriteRenderer_Shadow = this.gameObject.transform.Find("Bush Shadow").GetComponent<SpriteRenderer>(); // 몬스터 그림자 스프라이트 랜더러(이미지 + 색상 정보 등) 존재하지 않음
         m_tTransform = this.gameObject.GetComponent<Transform>();
         m_rRigdbody = this.gameObject.GetComponent<Rigidbody2D>();
 
+        m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.IDLE);
+        
         m_vRightPos = new Vector3(1, 1, 1);
         m_vLeftPos = new Vector3(-1, 1, 1);
-
-        m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.IDLE);
-
         m_bFix = false;
 
+        m_bAttack = true;
+        m_fPeacefulTime = 10f;
+        
+        m_FadeinAlpa = 0;
         if (m_sSpriteRenderer != null)
         {
             m_fAlpa = m_sSpriteRenderer.color.a;
         }
-        m_FadeinAlpa = 0;
-
-        m_fPeacefulTime = 10f;
-
-        m_bAttack = true;
     }
 }
