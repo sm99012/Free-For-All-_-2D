516f4a3d199759740f2493771f6020054a5bc956
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-23 15:59:06 +0900

    Update Bush2_Drop.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush2/Bush2_Drop.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush2/Bush2_Drop.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bush2_Drop : Bush1_Drop
+//
+// ※ 몬스터 제거(토벌 + 놓아주기) 시 플레이어가 획득 가능한 아이템을 관리하는 Monster_Drop 클래스는 MonsterManager 클래스로 통합되었다.
+//
+
+public class Bush2_Drop : Bush1_Drop // 기반이 되는 Bush1_Drop 클래스 상속
 {
 
 }
