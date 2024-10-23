817ebda1c8c73047425f2b17a8133edafdad150a
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-23 15:00:44 +0900

    Update Bush1_2_Status.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush1_2/Bush1_2_Status.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush1_2/Bush1_2_Status.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bush1_2_Status : Bush1_Status
+//
+// ※ 해당 클래스는 Bush1_Status 클래스를 상속해 몬스터("수풀")의 스탯(능력치)만을 다르게 조정한 클래스이다. 따라서 스탯(능력치)을 제외한 다른 기능은 모두 Bush1_Status 클래스와 동일하다.
+//    동일한 몬스터("수풀")임에도 각각 다른 스탯(능력치)을 보유하도록 몬스터("수풀")를 다양화 했다.
+//
+
+public class Bush1_2_Status : Bush1_Status // 기반이 되는 Bush1_Status 클래스 상속
 {
+    // 몬스터 스탯(능력치) 초기 설정
     override public void InitialSet_STATUS()
     {
         m_sStatus = new STATUS(1, 0, 0, 7, 7);
