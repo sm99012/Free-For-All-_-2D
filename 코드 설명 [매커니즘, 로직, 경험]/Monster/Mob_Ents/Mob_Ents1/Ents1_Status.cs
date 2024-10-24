81a180d789489f66f84b2f4dd0fb3790c56a470d
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-24 17:05:35 +0900

    Update Ents1_Status.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Ents/Mob_Ents1/Ents1_Status.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Ents/Mob_Ents1/Ents1_Status.cs
@@ -45,7 +45,7 @@ public class Ents1_Status : Monster_Status // 기반이 되는 Monster_Status �
     // 몬스터 피격 시 스탯(능력치(몬스터 현재체력)) 계산 함수 - 부모 클래스인 Monster_Status의 Operator_HP() 함수를 사용한다.
     // bool Operator_HP(int dm) {ㆍㆍㆍ}
 
-    // 몬스터 놓아주기 관련 함수
+    // 몬스터 놓아주기 함수
     override public void Goaway()
     {
         m_bPower = true;
