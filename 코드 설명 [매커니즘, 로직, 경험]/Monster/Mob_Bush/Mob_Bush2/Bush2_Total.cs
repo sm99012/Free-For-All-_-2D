b49614c9a55bab8ccd3616b4fb32133abce952dc
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-24 13:07:40 +0900

    Update Bush2_Total.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush2/Bush2_Total.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Bush/Mob_Bush2/Bush2_Total.cs
@@ -19,7 +19,7 @@ public class Bush2_Total : Bush1_Total // 기반이 되는 Bush1_Total 클래스
     
     void Update()
     {
-        if (m_bPlay == true) // 몬스터 동작 가능할 경우
+        if (m_bPlay == true) // 몬스터 동작 가능
         {
             if (m_bRelation == true && m_bWait == false) // 몬스터 접촉 시 오브젝트 피격 가능 && 다른 오브젝트와 상호작용 가능
             {
