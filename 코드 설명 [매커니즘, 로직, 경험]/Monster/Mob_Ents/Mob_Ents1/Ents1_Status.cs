997a529fd433e5dcbe74d37419ba59550cfdb89b
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-24 13:00:52 +0900

    Update Ents1_Status.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Ents/Mob_Ents1/Ents1_Status.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Ents/Mob_Ents1/Ents1_Status.cs
@@ -2,40 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ents1_Status : Monster_Status
+public class Ents1_Status : Monster_Status // 기반이 되는 Monster_Status 클래스 상속
 {
-    
     void Start()
     {
+        m_sMonsterName = "짙은 앤트";
+        
+        m_nMonsterCode = 3;
+        
         m_eObject_State = E_OBJECT_STATE.MONSTER;
         m_eMonster_Kind = E_MONSTER_KIND.ENTS;
 
-        InitialSet_SOC();
-        InitialSet_STATUS();
-
-        m_sMonsterName = "짙은 앤트";
-        m_nMonsterCode = 3;
+        InitialSet_SOC(); // 몬스터 스탯(평판) 초기 설정
+        InitialSet_STATUS(); // 몬스터 스탯(능력치) 초기 설정
     }
 
+    // 몬스터 스탯(평판) 초기 설정
     override public void InitialSet_SOC()
     {
         m_sSoc_null = new SOC();
         m_sSoc_Goaway = new SOC(0, 0, 0, 0, 0, 0, 0, 0, 0);
         m_sSoc_Death = new SOC(0, 0, 0, 0, 0, 0, 0, 0, 0);
     }
-
+    // 몬스터 스탯(능력치) 초기 설정
     override public void InitialSet_STATUS()
     {
-        //m_sStatus = new STATUS(3, 0, 5, 40, 40, 0, 0, 5, 5, 0, 0, 0, 20, 7, 7, 0, 5);
-        m_sStatus = new STATUS(12, 0, 7, 80, 80, 0, 0, 8, 8, 0, 0, 0, 15, 15, 15, 0, 5);
+        m_sStatus = new STATUS(12, 0, 13, 80, 80, 0, 0, 8, 8, 0, 0, 0, 15, 15, 15, 0, 5); // 레벨 : 12
+                                                                                          // 최대체력 : 80
+                                                                                          // 데미지(총데미지) : 8
+                                                                                          // 이동속도 : 15
+                                                                                          // 방어력(물리방어력) : 15
+                                                                                          // 공격속도 : 5
         m_sStatus_Origin.SetSTATUS(m_sStatus);
         m_sStatus_Goaway = new STATUS();
-        //m_sStatus_Death = new STATUS(0, 0, 3);
-        m_sStatus_Death = new STATUS(0, 0, 13);
+        m_sStatus_Death = new STATUS(0, 0, 13); // 몬스터 토벌 시 획득 가능한 스탯(능력치)
+                                                // 현재경험치 + 13
     }
 
+    // 몬스터 피격 시 스탯(능력치) 변동 함수 - 부모 클래스인 Monster_Status의 Attacked() 함수를 사용한다.
+    // virtual public bool Attacked(int dm, float dmrate) {ㆍㆍㆍ}
+
+    // 몬스터 피격 시 스탯(능력치(몬스터 현재체력)) 계산 함수 - 부모 클래스인 Monster_Status의 Operator_HP() 함수를 사용한다.
+    // bool Operator_HP(int dm) {ㆍㆍㆍ}
+
+    // 몬스터 놓아주기 관련 함수
     override public void Goaway()
     {
         m_bPower = true;
     }
+
+    // 몬스터 리스폰 함수 - 부모 클래스인 Monster_Status의 Respone() 함수를 사용한다.
+    // virtual public void Respone() {ㆍㆍㆍ}
 }
