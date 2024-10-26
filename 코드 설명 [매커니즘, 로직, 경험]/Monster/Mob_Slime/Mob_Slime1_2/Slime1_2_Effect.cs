034abbdcea539331d3df6979eb9878258fef6d5c
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-26 18:46:38 +0900

    Update Slime1_2_Effect.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime1_2/Slime1_2_Effect.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime1_2/Slime1_2_Effect.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class Slime1_2_Effect : Slime1_Effect
 {
+    // 부모 클래스인 Monster_Effect의 Start() 함수를 사용한다.
+    // void Start() {ㆍㆍㆍ}
+    
+    // 몬스터 놓아주기 이펙트 연출 함수 - 부모 클래스인 Monster_Effect의 Effect_Goaway() 함수를 사용한다.
+    // virtual public void Effect_Goaway(Vector3 pos) {ㆍㆍㆍ}
+    // 몬스터 놓아주기 이펙트 연출 코루틴 - 부모 클래스인 Monster_Effect의 ProcessEffect_Goaway() 코루틴을 사용한다.
+    // virtual protected IEnumerator ProcessEffect_Goaway(Vector3 pos) {ㆍㆍㆍ}
 
+    // 몬스터 ㆍㆍㆍ 이펙트 연출 함수 - "초원 슬라임"은 별도의 추가 이펙트를 가지지 않는다.
+    override public void Effect1(Vector3 pos) { }
+    // 몬스터 ㆍㆍㆍ 이펙트 연출 코루틴 - "초원 슬라임"은 별도의 추가 이펙트를 가지지 않는다.
+    override public void Effect1(Vector3 pos, int damage, string attackname) { }
 }
