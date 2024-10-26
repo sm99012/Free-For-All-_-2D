1e017cbc1dd73d249e969f9f430ee4881a193f90
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-26 18:46:15 +0900

    Update Slime1_2_Drop.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime1_2/Slime1_2_Drop.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime1_2/Slime1_2_Drop.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Slime1_2_Drop : Slime1_Drop
+//
+// ※ 몬스터 제거(토벌 + 놓아주기) 시 플레이어가 획득 가능한 아이템을 관리하는 Monster_Drop 클래스는 MonsterManager 클래스로 통합되었다.
+//
+
+public class Slime1_2_Drop : Slime1_Drop // 기반이 되는 Slime1_Drop 클래스 상속
 {
 
 }
