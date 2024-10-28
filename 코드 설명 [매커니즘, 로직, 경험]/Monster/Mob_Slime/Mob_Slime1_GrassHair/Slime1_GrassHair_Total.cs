c2964312da1d0ad95cef2d1cb9facf69ab1cf210
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-28 14:31:05 +0900

    Update Slime1_GrassHair_Total.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime1_GrassHair/Slime1_GrassHair_Total.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime1_GrassHair/Slime1_GrassHair_Total.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//
+// ※ "잔디 머리 초원 슬라임"은 이동형 몬스터로 설계했다. 평범한 속도로 이동하며 약하다. 흔히 볼 수 있다.
+//    "잔디 머리 초원 슬라임"은 공격 시 몸을 뻗어 오브젝트(플레이어)와 충돌하는 공격을 한다.
+//
+
 public class Slime1_GrassHair_Total : Slime1_Total
 {
 
