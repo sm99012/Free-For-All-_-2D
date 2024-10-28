0fdea069c51ad129a20d70138269a3ff63a02410
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-28 14:23:20 +0900

    Update Slime1_GrassHair_Effect.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime1_GrassHair/Slime1_GrassHair_Effect.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime1_GrassHair/Slime1_GrassHair_Effect.cs
@@ -2,7 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Slime1_GrassHair_Effect : Slime1_Effect
+public class Slime1_GrassHair_Effect : Slime1_Effect // 기반이 되는 Monster_Effect 클래스 상속
 {
+    // 부모 클래스인 Monster_Effect의 Start() 함수를 사용한다.
+    // void Start() {ㆍㆍㆍ}
+    
+    // 몬스터 놓아주기 이펙트 연출 함수 - 부모 클래스인 Monster_Effect의 Effect_Goaway() 함수를 사용한다.
+    // virtual public void Effect_Goaway(Vector3 pos) {ㆍㆍㆍ}
+    // 몬스터 놓아주기 이펙트 연출 코루틴 - 부모 클래스인 Monster_Effect의 ProcessEffect_Goaway() 코루틴을 사용한다.
+    // virtual protected IEnumerator ProcessEffect_Goaway(Vector3 pos) {ㆍㆍㆍ}
 
+    // 몬스터 ㆍㆍㆍ 이펙트 연출 함수 - "잔디 머리 초원 슬라임"은 별도의 추가 이펙트를 가지지 않는다.
+    override public void Effect1(Vector3 pos) { }
+    // 몬스터 ㆍㆍㆍ 이펙트 연출 코루틴 - "잔디 머리 초원 슬라임"은 별도의 추가 이펙트를 가지지 않는다.
+    override public void Effect1(Vector3 pos, int damage, string attackname) { }
 }
