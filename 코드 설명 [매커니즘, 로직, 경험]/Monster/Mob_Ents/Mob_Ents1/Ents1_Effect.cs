eadeb71cdeb43624ec8f91ffdfa71746d6d8eae1
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-28 15:05:24 +0900

    Update Ents1_Effect.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Ents/Mob_Ents1/Ents1_Effect.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Ents/Mob_Ents1/Ents1_Effect.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//
+// ※ "짙은 앤트"는 공격 시 지면 아래로 뿌리를 뻗어 일정 시간 경과 후 오브젝트(플레이어)의 위치로 뿌리를 돌출 시키는 공격을 한다.
+//    이때 돌출되는 뿌리는 이펙트로 연출된다.
+//
+
 public class Ents1_Effect : Monster_Effect // 기반이 되는 Monster_Effect 클래스 상속
 {
     void Start()
@@ -19,11 +24,6 @@ public class Ents1_Effect : Monster_Effect // 기반이 되는 Monster_Effect �
     // virtual public void Effect_Goaway(Vector3 pos) {ㆍㆍㆍ}
     // 몬스터 놓아주기 이펙트 연출 코루틴 - 부모 클래스인 Monster_Effect의 ProcessEffect_Goaway() 코루틴 사용한다.
     // virtual protected IEnumerator ProcessEffect_Goaway(Vector3 pos) {ㆍㆍㆍ}
-
-    //
-    // ※ "짙은 앤트"는 공격 시 지면 아래로 뿌리를 뻗어 일정 시간 경과 후 오브젝트(플레이어)의 위치로 뿌리를 돌출 시키는 공격을 한다.
-    //    이때 돌출되는 뿌리는 이펙트로 연출된다.
-    //
     
     // "짙은 앤트"의 공격 이펙트 연출 함수
     override public void Effect1(Vector3 pos, int damage, string attackname) // pos : 이펙트 위치, damage : 이펙트 데미지(충돌 시 피격), attackname : 피격 정보(이름)
