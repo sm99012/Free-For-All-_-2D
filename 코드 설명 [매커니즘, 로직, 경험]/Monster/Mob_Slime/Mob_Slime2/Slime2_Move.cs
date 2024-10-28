4e2114503117fc745313af6e8b296ecc42cfe785
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-28 20:45:27 +0900

    Update Slime2_Move.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime2/Slime2_Move.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime2/Slime2_Move.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Slime2_Move : Monster_Move
+public class Slime2_Move : Monster_Move // 기반이 되는 Monster_Move 클래스 상속
 {
-    // Start is called before the first frame update
     void Awake()
     {
         m_sSpriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
@@ -14,50 +13,52 @@ public class Slime2_Move : Monster_Move
 
         m_vRightPos = new Vector3(1, 1, 1);
         m_vLeftPos = new Vector3(-1, 1, 1);
-
-        m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.IDLE);
         m_bFix = false;
 
+        m_bAttack = true;
+        m_fPeacefulTime = 10f; // CHASE 상태에서 IDLE 상태로 전환되는 시간 : 10초
+        
+        m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.IDLE);
+        
         if (m_sSpriteRenderer != null)
         {
             m_fAlpa = m_sSpriteRenderer.color.a;
         }
         m_FadeinAlpa = 0;
-
-        m_fPeacefulTime = 10f;
-
-        m_bAttack = true;
     }
 
     void Start()
     {
-        Fadein();
+        Fadein(); // Fadein 효과 연출 함수
     }
 
-    override public void Move(int speed, Vector3 dir)
+    // 몬스터 이동 함수 - "큰 초원 슬라임"은 매우 느린 속도로 이동한다.
+    override public void Move(int speed, Vector3 dir) // speed : 몬스터 이동속도, dir : 몬스터 이동방향
     {
-        SetDir(dir);
+        SetDir(dir); // 몬스터 방향 설정 함수
 
         if (dir.x == 0 && dir.y == 0)
-            m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.IDLE);
+            m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.IDLE); // 몬스터 동작 FSM 변경 함수
         else
-            m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.RUN);
+            m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.RUN); // 몬스터 동작 FSM 변경 함수
 
-        if (m_eMonsterState == E_MONSTER_MOVE_STATE.RUN)
+        if (m_eMonsterState == E_MONSTER_MOVE_STATE.RUN) // 몬스터 동작 FSM 상태 판단
             if (m_bFix == false)
                 m_tTransform.position += (dir * speed * Time.deltaTime * 0.005f);
-                //m_rRigdbody.MovePosition(this.transform.position + (dir * speed * Time.deltaTime * 0.01f));
     }
-
+    
+    // 몬스터 추격 함수 - "큰 초원 슬라임"은 매우 느린 속도로 오브젝트(플레이어)를 추격한다.
     override public void Chase(int speed, Vector3 dir)
     {
-        SetDir(dir);
+        SetDir(dir); // 몬스터 방향 설정 함수
 
         if (m_bFix == false)
             m_tTransform.position += (dir * speed * Time.deltaTime * 0.005f);
-            //m_rRigdbody.MovePosition(this.transform.position + (dir * speed * Time.deltaTime * 0.01f));
     }
-
+    // 몬스터 추격 시간 계산 코루틴. 몬스터 동작 FSM : 추격(CHASE) -> 평화(IDLE) - 부모 클래스인 Monster_Move의 ProcessPeaceful() 코루틴을 사용한다.
+    // virtual protected IEnumerator ProcessPeaceful() {ㆍㆍㆍ}
+    
+    // 몬스터 방향 설정 함수
     override public void SetDir(Vector3 dir)
     {
         if (dir.x >= 0)
@@ -66,36 +67,63 @@ public class Slime2_Move : Monster_Move
             m_tTransform.localScale = m_vLeftPos;
     }
 
-    override public void Attacked()
+    // 몬스터 공격 함수 - "큰 초원 슬라임"은 공격 시 몸을 뻗어 오브젝트(플레이어)와 충돌하는 공격을 한다.
+    // return true : 몬스터 공격 성공 / return false : 몬스터 공격 실패
+    override public bool Attack(float attackspeed) // attackspeed : 몬스터 공격속도
     {
-        if (m_eMonsterState == E_MONSTER_MOVE_STATE.IDLE || m_eMonsterState == E_MONSTER_MOVE_STATE.RUN ||
-            m_eMonsterState == E_MONSTER_MOVE_STATE.CHASE || m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACKED)
-            m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.ATTACKED);
-    }
-
-    override public void Goaway()
-    {
-        if (m_eMonsterState == E_MONSTER_MOVE_STATE.IDLE || m_eMonsterState == E_MONSTER_MOVE_STATE.RUN)
-            m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.GOAWAY);
-    }
-
-    override public void Death()
-    {
-        m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.DEATH);
-    }
-
-    override public bool Attack(float attackspeed)
-    {
-        if (m_bAttack == true)
+        if (m_bAttack == true) // 몬스터 공격 가능
         if (m_eMonsterState == E_MONSTER_MOVE_STATE.CHASE ||
-            m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACKED)
+            m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACKED) // 몬스터 동작 FSM 상태 판단
         {
-            m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.ATTACK, attackspeed);
+            m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.ATTACK, attackspeed); // 몬스터 동작 FSM 변경 함수
             return true;
         }
         return false;
     }
+    // 몬스터 공격속도 계산 코루틴. 몬스터의 공격속도에 따라 다음 공격까지 기다려야하는 시간을 계산한다. - 부모 클래스인 Monster_Move의 ProcessAttack() 코루틴을 사용한다.
+    // virtual protected IEnumerator ProcessAttack(float attackspeed) {ㆍㆍㆍ}
+    // 몬스터 공격 종료 함수(가상 함수). 몬스터 공격 애니메이션의 특정 프레임에서 호출된다. - 부모 클래스인 Monster_Move의 EndAttack() 함수를 사용한다.
+    // virtual protected void EndAttack()
+    
+    // 몬스터 피격 함수
+    override public void Attacked()
+    {
+        if (m_eMonsterState == E_MONSTER_MOVE_STATE.IDLE || m_eMonsterState == E_MONSTER_MOVE_STATE.RUN ||
+            m_eMonsterState == E_MONSTER_MOVE_STATE.CHASE || m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACKED) // 몬스터 동작 FSM 상태 판단
+            m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.ATTACKED); // 몬스터 동작 FSM 변경 함수
+    }
+    // 몬스터 피격 시간 계산 코루틴1 - 부모 클래스인 Monster_Move의 ProcessAttacked1() 코루틴을 사용한다.
+    // virtual protected IEnumerator ProcessAttacked1() {ㆍㆍㆍ}
+    // 몬스터 피격 시간 계산 코루틴2 - 부모 클래스인 Monster_Move의 ProcessAttacked2() 코루틴을 사용한다.
+    // virtual protected IEnumerator ProcessAttacked2() {ㆍㆍㆍ}
 
+    // 몬스터 사망 함수. Fadeout 효과 관련
+    override public void Death()
+    {
+        m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.DEATH); // 몬스터 동작 FSM 변경 함수
+    }
+    // 몬스터 사망 시간 계산 코루틴. Fadeout 효과 관련 계산 - 부모 클래스인 Monster_Move의 ProcessDeath() 코루틴을 사용한다.
+    // virtual public IEnumerator ProcessDeath() {ㆍㆍㆍ}
+
+    // 몬스터 놓아주기 함수
+    override public void Goaway()
+    {
+        if (m_eMonsterState == E_MONSTER_MOVE_STATE.IDLE || m_eMonsterState == E_MONSTER_MOVE_STATE.RUN) // 몬스터 동작 FSM 상태 판단
+            m_eMonsterState = SetMonsterMoveState(E_MONSTER_MOVE_STATE.GOAWAY); // 몬스터 동작 FSM 변경 함수
+    }
+    // 몬스터 놓아주기 시간 계산 코루틴. Fadeout 효과 관련 계산 - 부모 클래스인 Monster_Move의 ProcessGoaway() 코루틴을 사용한다.
+    // virtual public IEnumerator ProcessGoaway() {ㆍㆍㆍ}
+
+    // 몬스터 리스폰 함수 - 부모 클래스인 Monster_Move의 Respone() 함수를 사용한다.
+    // virtual public void Respone() {ㆍㆍㆍ}
+
+    // Fadein 효과 연출 함수(가상 함수) - 부모 클래스인 Monster_Move의 Fadein() 함수를 사용한다.
+    // virtual public void Fadein() {ㆍㆍㆍ}
+    // Fadein 효과 계산 코루틴 - 부모 클래스인 Monster_Move의 ProcessFadein() 코루틴을 사용한다.
+    // IEnumerator ProcessFadein() {ㆍㆍㆍ}
+
+    // 몬스터 동작 FSM 변경 함수
+    // 몬스터 동작 FSM의 상태는 무조건 아래 FSM 상태 변경 함수를 통해서 변경된다. 상태 변경에 따른 적절한 조치(함수 실행, 애니메이션 병경)가 이루어 진다.
     override public E_MONSTER_MOVE_STATE SetMonsterMoveState(E_MONSTER_MOVE_STATE ms, float attackspeed = 0)
     {
         switch (ms)
@@ -106,8 +134,8 @@ public class Slime2_Move : Monster_Move
                     {
                         if (m_eMonsterState == E_MONSTER_MOVE_STATE.RUN || m_eMonsterState == E_MONSTER_MOVE_STATE.CHASE ||
                             m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACK || m_eMonsterState == E_MONSTER_MOVE_STATE.DEATH ||
-                            m_eMonsterState == E_MONSTER_MOVE_STATE.GOAWAY)
-                            SetAnimationParameters("IDLE");
+                            m_eMonsterState == E_MONSTER_MOVE_STATE.GOAWAY) // 몬스터 동작 FSM 상태 판단
+                            SetAnimationParameters("IDLE"); // 애니메이션 변경 : IDLE
                     }
                 }
                 break;
@@ -115,8 +143,8 @@ public class Slime2_Move : Monster_Move
                 {
                     if (m_eMonsterState != ms)
                     {
-                        if (m_eMonsterState == E_MONSTER_MOVE_STATE.IDLE)
-                            SetAnimationParameters("RUN");
+                        if (m_eMonsterState == E_MONSTER_MOVE_STATE.IDLE) // 몬스터 동작 FSM 상태 판단
+                            SetAnimationParameters("RUN"); // 애니메이션 변경 : RUN
                     }
                 }
                 break;
@@ -124,22 +152,22 @@ public class Slime2_Move : Monster_Move
                 {
                     if (m_eMonsterState == E_MONSTER_MOVE_STATE.IDLE || m_eMonsterState == E_MONSTER_MOVE_STATE.RUN ||
                         m_eMonsterState == E_MONSTER_MOVE_STATE.CHASE || m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACK ||
-                        m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACKED)
+                        m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACKED) // 몬스터 동작 FSM 상태 판단
                     {
                         if (m_cProcessAttacked == null)
-                            m_cProcessAttacked = StartCoroutine(ProcessAttacked1());
-                        else
+                            m_cProcessAttacked = StartCoroutine(ProcessAttacked1()); // 몬스터 피격 시간 계산 코루틴1
+                        else // 몬스터가 이미 피격 중일 경우
                         {
                             StopCoroutine(m_cProcessAttacked);
-                            m_cProcessAttacked = StartCoroutine(ProcessAttacked2());
+                            m_cProcessAttacked = StartCoroutine(ProcessAttacked2()); // 몬스터 피격 시간 계산 코루틴2
                         }
 
                         if (m_cProcessPeaceful == null)
-                            m_cProcessPeaceful = StartCoroutine(ProcessPeaceful());
-                        else
+                            m_cProcessPeaceful = StartCoroutine(ProcessPeaceful()); // 몬스터 추격 시간 계산 코루틴
+                        else // 몬스터가 이미 오브젝트(플레이어)를 추격하는 중일 경우
                         {
                             StopCoroutine(m_cProcessPeaceful);
-                            m_cProcessPeaceful = StartCoroutine(ProcessPeaceful());
+                            m_cProcessPeaceful = StartCoroutine(ProcessPeaceful()); // 몬스터 추격 시간 계산 코루틴
                         }
                     }
                 }
@@ -148,12 +176,12 @@ public class Slime2_Move : Monster_Move
                 {
                     if (m_eMonsterState != ms)
                     {
-                        if (m_eMonsterState == E_MONSTER_MOVE_STATE.CHASE)
+                        if (m_eMonsterState == E_MONSTER_MOVE_STATE.CHASE) // 몬스터 동작 FSM 상태 판단
                         {
-                            if (m_bAttack == true)
+                            if (m_bAttack == true) // 몬스터 공격 가능
                             {
-                                SetAnimationParameters("ATTACK");
-                                StartCoroutine(ProcessAttack(attackspeed));
+                                SetAnimationParameters("ATTACK"); // 애니메이션 변경 : ATTACK
+                                StartCoroutine(ProcessAttack(attackspeed)); // 몬스터 공격속도 계산 코루틴
                             }
                         }
                     }
@@ -165,12 +193,12 @@ public class Slime2_Move : Monster_Move
                     {
                         if (m_eMonsterState == E_MONSTER_MOVE_STATE.IDLE || m_eMonsterState == E_MONSTER_MOVE_STATE.RUN ||
                             m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACKED || m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACK ||
-                            m_eMonsterState == E_MONSTER_MOVE_STATE.CHASE)
+                            m_eMonsterState == E_MONSTER_MOVE_STATE.CHASE) // 몬스터 동작 FSM 상태 판단
                         {
-                            SetAnimationParameters("DEATH");
+                            SetAnimationParameters("DEATH"); // 애니메이션 변경 : DEATH
                             if (m_cProcessAttacked != null)
-                                StopCoroutine(m_cProcessAttacked);
-                            StartCoroutine(ProcessDeath());
+                                StopCoroutine(m_cProcessAttacked); // 몬스터 피격 시간 계산 코루틴 종료
+                            StartCoroutine(ProcessDeath()); // 몬스터 사망 시간 계산 코루틴
                         }
                     }
                 }
@@ -179,10 +207,10 @@ public class Slime2_Move : Monster_Move
                 {
                     if (m_eMonsterState != ms)
                     {
-                        if (m_eMonsterState == E_MONSTER_MOVE_STATE.IDLE || m_eMonsterState == E_MONSTER_MOVE_STATE.RUN)
+                        if (m_eMonsterState == E_MONSTER_MOVE_STATE.IDLE || m_eMonsterState == E_MONSTER_MOVE_STATE.RUN) // 몬스터 동작 FSM 상태 판단
                         {
-                            SetAnimationParameters("GOAWAY");
-                            StartCoroutine(ProcessGoaway());
+                            SetAnimationParameters("GOAWAY"); // 애니메이션 변경 : GOAWAY
+                            StartCoroutine(ProcessGoaway()); // 몬스터 놓아주기 시간 계산 코루틴
                         }
                     }
                 }
@@ -191,8 +219,8 @@ public class Slime2_Move : Monster_Move
                 {
                     if (m_eMonsterState != ms)
                     {
-                        if (m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACKED || m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACK)
-                            SetAnimationParameters("CHASE");
+                        if (m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACKED || m_eMonsterState == E_MONSTER_MOVE_STATE.ATTACK) // 몬스터 동작 FSM 상태 판단
+                            SetAnimationParameters("CHASE"); // 애니메이션 변경 : CHASE
                     }
                 }
                 break;
@@ -201,6 +229,7 @@ public class Slime2_Move : Monster_Move
         return ms;
     }
 
+    // 애니메이션 관리
     override public void SetAnimationParameters(string str)
     {
         switch (str)
