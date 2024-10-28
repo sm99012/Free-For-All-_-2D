aab775294f1182b1222786847b9da6eb8ee2c1a0
배종원 <75611409+sm99012@users.noreply.github.com>
2024-10-28 15:26:48 +0900

    Update Slime1_Total.cs

--- 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime1/Slime1_Total.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/Monster/Mob_Slime/Mob_Slime1/Slime1_Total.cs
@@ -203,7 +203,6 @@ public class Slime1_Total : Monster_Total // 기반이 되는 Monster_Total 클�
                 else
                     m_mm_Move.Attacked(); // 몬스터 피격 함수
 
-
                 return true;
             }
         }
