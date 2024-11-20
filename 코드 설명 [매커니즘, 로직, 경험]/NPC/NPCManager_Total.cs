29eaf104e34521da7a8823652b6336f47097f78a
배종원 <75611409+sm99012@users.noreply.github.com>
2024-11-20 16:57:59 +0900

    Update NPCManager_Total.cs

--- 코드 설명 [매커니즘, 로직, 경험]/NPC/NPCManager_Total.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/NPC/NPCManager_Total.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 //
 // ※ 싱글톤패턴을 적용한 NPCManager_Total 클래스를 이용해 현재 위치한 게임 씬 내부의 모든 NPC를 관리한다.
-//    해당 클래스에서는 5분마다 
+//    현재 위치한 게임 씬 내부 NPC의 퀘스트, 거래 상태 아이콘 업데이트, 거래 초기화 작업이 이루어진다.
 //
 
 public class NPCManager_Total : MonoBehaviour
@@ -21,9 +21,6 @@ public class NPCManager_Total : MonoBehaviour
             return instance;
         }
     }
-
-    public static Dictionary<int, NPC_Total> m_Dictionary_NPC;
-
     private void Awake()
     {
         if (instance == null)
@@ -38,19 +35,24 @@ public class NPCManager_Total : MonoBehaviour
 
         m_Dictionary_NPC = new Dictionary<int, NPC_Total>();
     }
+    
+    public static Dictionary<int, NPC_Total> m_Dictionary_NPC; // 현재 위치한 게임 씬 내부 NPC 딕셔너리. Dictionary <Key : NPC 고유코드, Value : NPC 객체>
 
-    static float m_fUpdateTime = 300f;
-    static float m_fCurrentTime = 0.0f;
+    // NPC가 보유한 거래 초기화 작업을 위한 타이머 변수
+    static float m_fUpdateTime = 300f;  // 300초(5분)
+    static float m_fCurrentTime = 0.0f; // 현재 시간(초)
+    
     private void Update()
     {
+        // 5분마다 거래 초기화 작업 수행
         if (m_fCurrentTime < m_fUpdateTime)
         {
             m_fCurrentTime += Time.deltaTime;
         }
         else
         {
-            m_fCurrentTime = 0.0f;
-            UpdateNPC_Fix();
+            m_fCurrentTime = 0.0f; // 현재 시간 초기화
+            UpdateNPC_Fix(); // 
         }
 
         if (GUIManager_Total.Instance.m_GUI_Store != null)
@@ -75,30 +77,33 @@ public class NPCManager_Total : MonoBehaviour
         }
     }
 
+    // 현재 위치한 게임 씬 내부 NPC의 거래 갱신 함수. NPC가 보유한 거래가 초기화(갱신) 된다. 판매 아이템 종류, 수량, 매매 가격 등이 변경된다.
     static void UpdateNPC_Fix()
     {
         foreach (KeyValuePair<int, NPC_Total> dictionary in m_Dictionary_NPC)
         {
             if (dictionary.Value != null)
             {
-                dictionary.Value.UpdateIcon();
+                dictionary.Value.UpdateIcon(); // NPC가 보유한 퀘스트, 거래 상태를 알려주기 위한 아이콘 갱신 함수(가상 함수)
                 if (dictionary.Value.m_bNPC_Store == true)
                 {
                     for (int i = 0; i < dictionary.Value.m_List_NPC_Store.Count; i++)
                     {
-                        if (dictionary.Value.m_List_NPC_Store[i].Check_Condition_Store() == true)
+                        if (dictionary.Value.m_List_NPC_Store[i].Check_Condition_Store() == true) // 거래 이용 사전 조건 판단 함수 == true : 거래 이용 가능
                         {
-                            dictionary.Value.Update_Store();
-                            Debug.Log("[" + dictionary.Value.m_sNPCName + "] 판매 목록 갱신.");
+                            dictionary.Value.Update_Store(); // NPC가 보유한 거래 갱신 함수
+                            // Debug.Log("[" + dictionary.Value.m_sNPCName + "] 판매 목록 갱신.");
                         }
                     }
                 }
             }
         }
-        if (GUIManager_Total.Instance.m_GUI_Store.m_gPanel_Store.activeSelf == true)
+
+        // 플레이어가 NPC와 거래를 진행중일때 거래 초기화가 일어날 경우 해당 거래 종료
+        if (GUIManager_Total.Instance.m_GUI_Store.m_gPanel_Store.activeSelf == true) // 거래 GUI 상태 == true : 거래 GUI 활성화 상태
         {
-            GUIManager_Total.Instance.m_GUI_Store.Press_Btn_Exit();
-            GUIManager_Total.Instance.Display_GUI_Store_Exit_Information();
+            GUIManager_Total.Instance.m_GUI_Store.Press_Btn_Exit(); // 거래 종료(거래 GUI 비활성화)
+            GUIManager_Total.Instance.Display_GUI_Store_Exit_Information(); // 거래 종료(거래 초기화 알림 GUI 활성화)
         }
     }
 }
