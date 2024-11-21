14b16f8f4a3875a1518e2224040ab2002761df28
배종원 <75611409+sm99012@users.noreply.github.com>
2024-11-21 10:32:23 +0900

    Update GUI_Player.cs

--- 코드 설명 [매커니즘, 로직, 경험]/GUI/GUI_Player/GUI_Player.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/GUI/GUI_Player/GUI_Player.cs
@@ -4,38 +4,35 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+// ※ 플레이어 스탯(능력치(체력, 마나, 경험치)) GUI
+
 public class GUI_Player : MonoBehaviour
 {
+    // GUI 오브젝트
     [SerializeField] GameObject m_gPanel_PlayerStatus;
     [SerializeField] GameObject m_gPanel_PlayerStatus_HPBar;
     [SerializeField] GameObject m_gPanel_PlayerStatus_MPBar;
+    
     [Space(20)]
     [SerializeField] GameObject m_gPanel_DownBar;
 
-    [Space(20)]
-    TextMeshProUGUI m_TMP_LV;
-    TextMeshProUGUI m_TMP_HP;
-    TextMeshProUGUI m_TMP_MP;
-    TextMeshProUGUI m_TMP_EXP;
+    // 플레이어 스탯(능력치(체력, 마나, 경험치)) 텍스트 관련 변수
+    TextMeshProUGUI m_TMP_LV;  // 플레이어 레벨
+    TextMeshProUGUI m_TMP_HP;  // 플레이어 현재체력 / 최대체력
+    TextMeshProUGUI m_TMP_MP;  // 플레이어 현재마나 / 최대마나
+    TextMeshProUGUI m_TMP_EXP; // 플레이어 현재경험치 / 최대경험치
 
-    Image m_IMG_HP;
-    Image m_IMG_MP;
-    Image m_IMG_EXP;
+    // 플레이어 스탯(능력치(체력, 마나, 경험치)) 이미지 관련 변수
+    Image m_IMG_HP;  // 체력 이미지
+    Image m_IMG_MP;  // 마나 이미지
+    Image m_IMG_EXP; // 경험치 이미지
 
+    // GUI 초기 설정
     public void InitialSet()
     {
-        InitialSet_Object();
+        InitialSet_Object(); // GUI 오브젝트 초기 설정
     }
-
-    private void Update()
-    {
-        if (Total_Manager.Instance.m_bStart == true)
-            if (Player_Total.Instance != null)
-                UpdatePlayerStatusUI();
-    }
-
-
-    // 초기 Object 불러오기.
+    // GUI 오브젝트 초기 설정
     void InitialSet_Object()
     {
         m_gPanel_PlayerStatus = GameObject.Find("Panel_PlayerStatus");
@@ -43,31 +40,43 @@ public class GUI_Player : MonoBehaviour
         m_gPanel_PlayerStatus_HPBar = m_gPanel_PlayerStatus.transform.Find("Panel_PlayerStatus_HPBar").gameObject;
         m_IMG_HP = m_gPanel_PlayerStatus_HPBar.transform.Find("IMG_PlayerStatus_HPBar_Current").gameObject.GetComponent<Image>();
         m_TMP_HP = m_gPanel_PlayerStatus_HPBar.transform.Find("TMP_PlayerStatus_HPBar").gameObject.GetComponent<TextMeshProUGUI>();
+        
         m_gPanel_PlayerStatus_MPBar = m_gPanel_PlayerStatus.transform.Find("Panel_PlayerStatus_MPBar").gameObject;
         m_IMG_MP = m_gPanel_PlayerStatus_MPBar.transform.Find("IMG_PlayerStatus_MPBar_Current").gameObject.GetComponent<Image>();
         m_TMP_MP = m_gPanel_PlayerStatus_MPBar.transform.Find("TMP_PlayerStatus_MPBar").gameObject.GetComponent<TextMeshProUGUI>();
-
 
         m_gPanel_DownBar = GameObject.Find("Panel_DownBar");
 
         m_IMG_EXP = m_gPanel_DownBar.transform.Find("IMG_DownBar_EXPBar_Current").gameObject.GetComponent<Image>();
         m_TMP_EXP = m_gPanel_DownBar.transform.Find("TMP_DownBar_EXPBar").gameObject.GetComponent<TextMeshProUGUI>();
+        
         m_TMP_LV = m_gPanel_DownBar.transform.Find("TMP_DownBar_LVBar").gameObject.GetComponent<TextMeshProUGUI>();
     }
 
+    private void Update()
+    {
+        if (Total_Manager.Instance.m_bStart == true)
+            if (Player_Total.Instance != null)
+                UpdatePlayerStatusUI(); // 플레이어 스탯(능력치(체력, 마나, 경험치)) GUI 업데이트
+    }
+
+    // 플레이어 스탯(능력치(체력, 마나, 경험치)) GUI 업데이트
     public void UpdatePlayerStatusUI()
     {
-        UpdateText();
-        UpdateImage();
+        UpdateText();  // 플레이어 스탯(능력치(체력, 마나, 경험치)) 텍스트 업데이트
+        UpdateImage(); // 플레이어 스탯(능력치(체력, 마나, 경험치)) 이미지 업데이트
     }
+    // 플레이어 스탯(능력치(체력, 마나, 경험치)) 텍스트 업데이트
     void UpdateText()
     {
         m_TMP_LV.text = "LV: " + Player_Total.Instance.m_ps_Status.m_sStatus.GetSTATUS_LV().ToString();
         m_TMP_HP.text = Player_Total.Instance.m_ps_Status.m_sStatus.GetSTATUS_HP_Current().ToString() + " / " + Player_Total.Instance.m_ps_Status.m_sStatus.GetSTATUS_HP_Max().ToString();
         m_TMP_MP.text = Player_Total.Instance.m_ps_Status.m_sStatus.GetSTATUS_MP_Current().ToString() + " / " + Player_Total.Instance.m_ps_Status.m_sStatus.GetSTATUS_MP_Max().ToString();
-        m_TMP_EXP.text = "EXP: " + ((float)Player_Total.Instance.m_ps_Status.m_sStatus.GetSTATUS_EXP_Current()/ (float)Player_Total.Instance.m_ps_Status.m_sStatus.GetSTATUS_EXP_Max()) * 100 + "% (" + 
+        m_TMP_EXP.text = "EXP: " + ((float)Player_Total.Instance.m_ps_Status.m_sStatus.GetSTATUS_EXP_Current() / (float)Player_Total.Instance.m_ps_Status.m_sStatus.GetSTATUS_EXP_Max()) * 100 + "% (" + 
             Player_Total.Instance.m_ps_Status.m_sStatus.GetSTATUS_EXP_Current().ToString() + " / " + Player_Total.Instance.m_ps_Status.m_sStatus.GetSTATUS_EXP_Max().ToString() + ")";
     }
+    // 플레이어 스탯(능력치(체력, 마나, 경험치)) 이미지 업데이트
+    // 선형보간 함수(Mathf.Lerp())를 사용해 플레이어 스탯(능력치(체력, 마나, 경험치)) 이미지의 부드러운 변화를 구현했다.
     void UpdateImage()
     {
         if (Player_Total.Instance.m_ps_Status.m_sStatus.GetSTATUS_HP_Max() != 0)
