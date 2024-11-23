d5fbc0215109d5decea5c2d05bf179dae29019ef
배종원 <75611409+sm99012@users.noreply.github.com>
2024-11-24 06:58:55 +0900

    Update GUI_Equipslot_Remove_Information.cs

--- 코드 설명 [매커니즘, 로직, 경험]/GUI/GUI_Player/GUI_Equipslot/GUI_Equipslot_Remove_Information.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/GUI/GUI_Player/GUI_Equipslot/GUI_Equipslot_Remove_Information.cs
@@ -5,23 +5,25 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using TMPro;
 
+//
+// ※ 장비아이템 착용 해제 알림 GUI
+//    플레이어의 스탯(능력치, 평판) 변동으로 인해 착용중인 장비아이템이 해제될 경우 활성화된다.
+//
 
 public class GUI_Equipslot_Remove_Information : MonoBehaviour
 {
-    // Equipslot_Remove_Information 에서 재확인 하는 장비 아이템 착용 해제 관련 UI.
+    // GUI 오브젝트
     [SerializeField] GameObject m_gPanel_Equipslot_Remove_Information;
-    [Space(20)]
-
     [SerializeField] GameObject m_gPanel_Equipslot_Remove_Information_Content;
-    [SerializeField] Button m_BTN_Equipslot_Remove_Information_Content_Ok;
+    [SerializeField] Button m_BTN_Equipslot_Remove_Information_Content_Ok; // (버튼) GUI 비활성화
 
+    // GUI 초기 설정
     public void InitialSet()
     {
-        InitialSet_Object();
-        InitialSet_Button();
+        InitialSet_Object(); // GUI 오브젝트 초기 설정
+        InitialSet_Button(); // GUI 버튼 설정
     }
-
-    // 초기 Object 불러오기.
+    // GUI 오브젝트 초기 설정
     void InitialSet_Object()
     {
         m_gPanel_Equipslot_Remove_Information = GameObject.Find("Canvas_GUI").transform.Find("Panel_Equipslot_Remove_Information").gameObject;
@@ -29,14 +31,15 @@ public class GUI_Equipslot_Remove_Information : MonoBehaviour
         m_gPanel_Equipslot_Remove_Information_Content = m_gPanel_Equipslot_Remove_Information.transform.Find("Panel_Equipslot_Remove_Information_Content").gameObject;
         m_BTN_Equipslot_Remove_Information_Content_Ok = m_gPanel_Equipslot_Remove_Information_Content.transform.Find("BTN_Equipslot_Remove_Information_Content_Ok").gameObject.GetComponent<Button>();
     }
-    // 초기 Button 이벤트 설정.
+    // GUI 버튼 설정
     void InitialSet_Button()
     {
+        // (버튼) GUI 비활성화 클릭 이벤트 함수 설정
         m_BTN_Equipslot_Remove_Information_Content_Ok.onClick.RemoveAllListeners();
         m_BTN_Equipslot_Remove_Information_Content_Ok.onClick.AddListener(delegate { Btn_Press_Ok(); });
     }
 
-    // 버튼 이벤트 처리.
+    // (버튼) GUI 비활성화 클릭 이벤트 함수 - 장비아이템 착용 해제 알림 GUI를 비활성화 한다.
     void Btn_Press_Ok()
     {
         m_gPanel_Equipslot_Remove_Information.SetActive(false);
