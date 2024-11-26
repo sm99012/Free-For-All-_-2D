6ae56a4dfab1dfe43ae2ad8d0b88bd34c4576862
배종원 <75611409+sm99012@users.noreply.github.com>
2024-11-27 08:49:47 +0900

    Update GUI_Itemslot.cs

--- 코드 설명 [매커니즘, 로직, 경험]/GUI/GUI_Player/GUI_Itemslot/GUI_Itemslot.cs
+++ 코드 설명 [매커니즘, 로직, 경험]/GUI/GUI_Player/GUI_Itemslot/GUI_Itemslot.cs
@@ -22,11 +22,11 @@ public class GUI_Itemslot : MonoBehaviour
     [SerializeField] GameObject m_gPanel_Itemslot_Content;
     
     [SerializeField] GameObject m_gPanel_Itemslot_Content_Category_Equip;
-    [SerializeField] Button m_BTN_Itemslot_Content_Category_Equip; // (버튼) 인벤토리_장비아이템
+    [SerializeField] Button m_BTN_Itemslot_Content_Category_Equip; // (버튼) 인벤토리 _ 장비아이템 선택
     [SerializeField] GameObject m_gPanel_Itemslot_Content_Category_Use;
-    [SerializeField] Button m_BTN_Itemslot_Content_Category_Use;   // (버튼) 인벤토리_소비아이템
+    [SerializeField] Button m_BTN_Itemslot_Content_Category_Use;   // (버튼) 인벤토리 _ 소비아이템 선택
     [SerializeField] GameObject m_gPanel_Itemslot_Content_Category_Etc;
-    [SerializeField] Button m_BTN_Itemslot_Content_Category_Etc;   // (버튼) 인벤토리_기타아이템
+    [SerializeField] Button m_BTN_Itemslot_Content_Category_Etc;   // (버튼) 인벤토리 _ 기타아이템 선택
         
     [SerializeField] GameObject m_gPanel_Itemslot_Content_Slot;
     [SerializeField] GameObject m_gContent_Itemslot_Content_Slot;
@@ -37,10 +37,10 @@ public class GUI_Itemslot : MonoBehaviour
     [SerializeField] GameObject m_gPanel_Itemslot_Content_Gold;
     [SerializeField] TextMeshProUGUI m_TMP_Itemslot_Content_Gold; // (텍스트) 골드(재화)
 
-    // 인벤토리 타입 : { 인벤토리_장비아이템, 인벤토리_소비아이템, 인벤토리_기타아이템 }
+    // 인벤토리 타입 : { 인벤토리 _ 장비아이템, 인벤토리 _ 소비아이템, 인벤토리 _ 기타아이템 }
     public enum E_ITEMSLOT { EQUIP, USE, ETC }
     public E_ITEMSLOT m_eItemslot = E_ITEMSLOT.EQUIP; // 인벤토리 타입
-                                                      // 인벤토리_장비아이템, 인벤토리_소비아이템, 인벤토리_기타아이템 마다 각각의 인벤토리 슬롯 GUI를 가지는것이 아니다.(만약 각각 인벤토리 슬롯 GUI를 가진다면 180개가 필요하다.)
+                                                      // 인벤토리 _ 장비아이템, 인벤토리 _ 소비아이템, 인벤토리 _ 기타아이템 마다 각각의 인벤토리 슬롯 GUI를 가지는것이 아니다.(만약 각각 인벤토리 슬롯 GUI를 가진다면 180개가 필요하다.)
                                                       // 60개의 인벤토리 슬롯 GUI를 이용해 각각의 인벤토리 타입에 맞게 재활용한다.
 
     public GameObject[] m_gary_Itemslot; // 인벤토리 슬롯 GUI 배열. 60개(0 ~ 59) 인벤토리 슬롯 GUI가 저장된다.
@@ -63,14 +63,12 @@ public class GUI_Itemslot : MonoBehaviour
         m_gPanel_Itemslot_Exit = m_gPanel_Itemslot.transform.Find("Panel_Itemslot_Exit").gameObject;
         m_BTN_Itemslot_Exit = m_gPanel_Itemslot_Exit.transform.Find("BTN_Itemslot_Exit").gameObject.GetComponent<Button>();
 
-
         m_gPanel_Itemslot_Content = m_gPanel_Itemslot.transform.Find("Panel_Itemslot_Content").gameObject;
+        
         m_gPanel_Itemslot_Content_Category_Equip = m_gPanel_Itemslot_Content.transform.Find("Panel_Itemslot_Content_Category_Equip").gameObject;
         m_BTN_Itemslot_Content_Category_Equip = m_gPanel_Itemslot_Content_Category_Equip.transform.Find("BTN_Itemslot_Content_Category_Equip").gameObject.GetComponent<Button>();
-
         m_gPanel_Itemslot_Content_Category_Use = m_gPanel_Itemslot_Content.transform.Find("Panel_Itemslot_Content_Category_Use").gameObject;
         m_BTN_Itemslot_Content_Category_Use = m_gPanel_Itemslot_Content_Category_Use.transform.Find("BTN_Itemslot_Content_Category_Use").gameObject.GetComponent<Button>();
-
         m_gPanel_Itemslot_Content_Category_Etc = m_gPanel_Itemslot_Content.transform.Find("Panel_Itemslot_Content_Category_Etc").gameObject;
         m_BTN_Itemslot_Content_Category_Etc = m_gPanel_Itemslot_Content_Category_Etc.transform.Find("BTN_Itemslot_Content_Category_Etc").gameObject.GetComponent<Button>();
 
@@ -78,19 +76,25 @@ public class GUI_Itemslot : MonoBehaviour
         m_gScrollView_Itemslot_Content_Slot = m_gPanel_Itemslot_Content_Slot.transform.Find("ScrollView_Itemslot_Content_Slot").gameObject;
         m_gViewport_Itemslot_Content_Slot = m_gScrollView_Itemslot_Content_Slot.transform.Find("Viewport_Itemslot_Content_Slot").gameObject;
         m_gContent_Itemslot_Content_Slot = m_gViewport_Itemslot_Content_Slot.transform.Find("Content_Itemslot_Content_Slot").gameObject;
+        m_gScrollbarVertical_Itemslot_Content_Slot = m_gScrollView_Itemslot_Content_Slot.transform.Find("ScrollbarVertical_Itemslot_Content_Slot").gameObject;
 
         m_gPanel_Itemslot_Content_Gold = m_gPanel_Itemslot_Content.transform.Find("Panel_Itemslot_Content_Gold").gameObject;
         m_TMP_Itemslot_Content_Gold = m_gPanel_Itemslot_Content_Gold.transform.Find("TMP_Itemslot_Content_Gold").gameObject.GetComponent<TextMeshProUGUI>();
-
-        m_gScrollbarVertical_Itemslot_Content_Slot = m_gScrollView_Itemslot_Content_Slot.transform.Find("ScrollbarVertical_Itemslot_Content_Slot").gameObject;
-
     }
     // GUI 버튼 설정
     void InitialSet_Button()
     {
+        // (버튼) 인벤토리 GUI 비활성화 클릭 이벤트 함수 설정
+        m_BTN_Itemslot_Exit.GetComponent<Button>().onClick.RemoveAllListeners();
         m_BTN_Itemslot_Exit.GetComponent<Button>().onClick.AddListener(delegate { Btn_Press_Exit(); });
+        // (버튼) 인벤토리 _ 장비아이템 선택 클릭 이벤트 함수 설정
+        m_BTN_Itemslot_Content_Category_Equip.GetComponent<Button>().onClick.RemoveAllListeners();
         m_BTN_Itemslot_Content_Category_Equip.GetComponent<Button>().onClick.AddListener(delegate { Btn_Press_Itemslot_Equip(); });
+        // (버튼) 인벤토리 _ 소비아이템 선택 클릭 이벤트 함수 설정
+        m_BTN_Itemslot_Content_Category_Use.GetComponent<Button>().onClick.RemoveAllListeners();
         m_BTN_Itemslot_Content_Category_Use.GetComponent<Button>().onClick.AddListener(delegate { Btn_Press_Itemslot_Use(); });
+        // (버튼) 인벤토리 _ 기타아이템 선택 클릭 이벤트 함수 설정
+        m_BTN_Itemslot_Content_Category_Etc.GetComponent<Button>().onClick.RemoveAllListeners();
         m_BTN_Itemslot_Content_Category_Etc.GetComponent<Button>().onClick.AddListener(delegate { Btn_Press_Itemslot_Etc(); });
     }
     // 인벤토리 슬롯 GUI 초기 설정
@@ -104,97 +108,151 @@ public class GUI_Itemslot : MonoBehaviour
         }
     }
 
+    // (버튼) 인벤토리 GUI 비활성화 클릭 이벤트 함수 - 인벤토리 GUI를 비활성화한다.
+    public void Btn_Press_Exit()
+    {
+        m_BTN_Itemslot_Exit.GetComponent<Image>().color = new Color(.75f, .75f, .75f, 1);
+        Display_GUI_Itemslot(); // 인벤토리 GUI 활성화 / 비활성화. 비활성화에 해당한다.
+        GUIManager_Total.Instance.Delete_GUI_Priority(8); // GUI 우선순위 제거
+    }
+    // (버튼) 인벤토리 _ 장비아이템 선택 클릭 이벤트 함수 - 인벤토리 _ 장비아이템을 활성화한다.
+    public void Btn_Press_Itemslot_Equip()
+    {
+        m_eItemslot = E_ITEMSLOT.EQUIP; // 인벤토리 타입 = 인벤토리 _ 장비아이템
+
+        UpdateItemslot(); // 인벤토리 GUI 업데이트
+        Itemslot_Reset(); // 인벤토리 관련 GUI 업데이트(초기화)
+
+        m_BTN_Itemslot_Content_Category_Equip.GetComponent<Image>().color = new Color(.75f, .75f, .75f, 1); // (버튼) 인벤토리 _ 장비아이템 선택 색 변경
+        m_BTN_Itemslot_Content_Category_Use.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        m_BTN_Itemslot_Content_Category_Etc.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+
+        m_gPanel_Itemslot.transform.SetAsLastSibling();
+    }
+    // (버튼) 인벤토리 _ 소비아이템 선택 클릭 이벤트 함수 - 인벤토리 _ 소비아이템을 활성화한다.
+    public void Btn_Press_Itemslot_Use()
+    {
+        m_eItemslot = E_ITEMSLOT.USE; // 인벤토리 타입 = 인벤토리 _ 소비아이템
+
+        UpdateItemslot(); // 인벤토리 GUI 업데이트
+        Itemslot_Reset(); // 인벤토리 관련 GUI 업데이트(초기화)
+
+        m_BTN_Itemslot_Content_Category_Equip.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        m_BTN_Itemslot_Content_Category_Use.GetComponent<Image>().color = new Color(.75f, .75f, .75f, 1); // (버튼) 인벤토리 _ 소비아이템 선택 색 변경
+        m_BTN_Itemslot_Content_Category_Etc.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+
+        m_gPanel_Itemslot.transform.SetAsLastSibling();
+    }
+    // (버튼) 인벤토리 _ 기타아이템 선택 클릭 이벤트 함수 - 인벤토리 _ 기타아이템을 활성화한다.
+    public void Btn_Press_Itemslot_Etc()
+    {
+        m_eItemslot = E_ITEMSLOT.ETC; // 인벤토리 타입 = 인벤토리 _ 기타아이템
+
+        UpdateItemslot(); // 인벤토리 GUI 업데이트
+        Itemslot_Reset(); // 인벤토리 관련 GUI 업데이트(초기화)
+
+        m_BTN_Itemslot_Content_Category_Equip.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        m_BTN_Itemslot_Content_Category_Use.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+        m_BTN_Itemslot_Content_Category_Etc.GetComponent<Image>().color = new Color(.75f, .75f, .75f, 1); // (버튼) 인벤토리 _ 기타아이템 선택 색 변경
+
+        m_gPanel_Itemslot.transform.SetAsLastSibling();
+    }
+
+    // 인벤토리 GUI 활성화 / 비활성화 함수
+    // return true : 인벤토리 GUI 활성화 / return false : 인벤토리 GUI 비활성화
     public bool Display_GUI_Itemslot()
     {
         if (m_gPanel_Itemslot.activeSelf == true)
         {
             m_gPanel_Itemslot.SetActive(false);
         
-            GUIManager_Total.Instance.UnDisplay_GUI_Itemslot_Equip_Information();
-            GUIManager_Total.Instance.UnDisplay_GUI_Itemslot_Use_Information();
-            GUIManager_Total.Instance.UnDisplay_GUI_Itemslot_Etc_Information();
+            GUIManager_Total.Instance.UnDisplay_GUI_Itemslot_Equip_Information(); // 인벤토리 장비아이템 세부 정보 GUI 비활성화
+            GUIManager_Total.Instance.UnDisplay_GUI_Itemslot_Use_Information();   // 인벤토리 소비아이템 세부 정보 GUI 비활성화
+            GUIManager_Total.Instance.UnDisplay_GUI_Itemslot_Etc_Information();   // 인벤토리 기타아이템 세부 정보 GUI 비활성화
 
-            GUIManager_Total.Instance.UnDisplay_GUI_Quickslot_Signdown();
-
-            m_TMP_Itemslot_Content_Gold.text = Player_Total.Instance.m_pi_Itemslot.m_nGold.ToString();
+            GUIManager_Total.Instance.UnDisplay_GUI_Quickslot_Signup(); // 퀵슬롯 등록 GUI 비활성화
 
             return false;
         }
         else
         {
-            m_BTN_Itemslot_Exit.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-            Itemslot_Reset();
             m_gPanel_Itemslot.SetActive(true);
+            
+            m_BTN_Itemslot_Exit.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+            
+            Itemslot_Reset(); // 인벤토리 관련 GUI 업데이트(초기화)
+            UpdateItemslot_Gold(); // 인벤토리 GUI 업데이트(골드(재화))
+            
             m_gPanel_Itemslot.transform.SetAsLastSibling();
 
             return true;
         }
     }
 
+    // 인벤토리 GUI 업데이트
     public void UpdateItemslot()
     {
-        //if (m_g_ItemslotBox.activeSelf == true)
+        switch (m_eItemslot) // 인벤토리 타입
         {
-            switch (m_eItemslot)
-            {
-                case E_ITEMSLOT.EQUIP:
+            // 인벤토리 _ 장비아이템
+            case E_ITEMSLOT.EQUIP:
+                {
+                    for (int i = 0; i < 60; i++)
                     {
-                        for (int i = 0; i < 60; i++)
+                        if (Player_Itemslot.m_nary_Itemslot_Equip_Count[i] != 0)
                         {
-                            if (Player_Itemslot.m_nary_Itemslot_Equip_Count[i] != 0)
+                            m_gary_Itemslot[i].GetComponent<Itemslot>().SetItem_Equip(Player_Itemslot.m_gary_Itemslot_Equip[i], Player_Itemslot.m_nary_Itemslot_Equip_Count[i]);
+                            if (Player_Itemslot.m_nary_Itemslot_Equip_Count[i] == 0)
                             {
-                                m_gary_Itemslot[i].GetComponent<Itemslot>().SetItem_Equip(Player_Itemslot.m_gary_Itemslot_Equip[i], Player_Itemslot.m_nary_Itemslot_Equip_Count[i]);
-                                if (Player_Itemslot.m_nary_Itemslot_Equip_Count[i] == 0)
-                                {
-                                    Player_Itemslot.m_gary_Itemslot_Equip[i] = null;
-                                }
-                            }
-                            else
-                                m_gary_Itemslot[i].GetComponent<Itemslot>().SetNull();
-                        }
-                    }
-                    break;
-                case E_ITEMSLOT.USE:
-                    {
-                        for (int i = 0; i < 60; i++)
-                        {
-                            if (Player_Itemslot.m_nary_Itemslot_Use_Count[i] != 0)
-                            {
-                                m_gary_Itemslot[i].GetComponent<Itemslot>().SetItem_Use(Player_Itemslot.m_gary_Itemslot_Use[i], Player_Itemslot.m_nary_Itemslot_Use_Count[i]);
-                                if (Player_Itemslot.m_nary_Itemslot_Use_Count[i] == 0)
-                                {
-                                    Player_Itemslot.m_gary_Itemslot_Use[i] = null;
-                                }
-                            }
-                            else
-                                m_gary_Itemslot[i].GetComponent<Itemslot>().SetNull();
-                        }
-                    }
-                    break;
-                case E_ITEMSLOT.ETC:
-                    {
-                        for (int i = 0; i < 60; i++)
-                        {
-                            if (Player_Itemslot.m_nary_Itemslot_Etc_Count[i] != 0)
-                            {
-                                m_gary_Itemslot[i].GetComponent<Itemslot>().SetItem_Etc(Player_Itemslot.m_gary_Itemslot_Etc[i], Player_Itemslot.m_nary_Itemslot_Etc_Count[i]);
-                                if (Player_Itemslot.m_nary_Itemslot_Etc_Count[i] == 0)
-                                {
-                                    Player_Itemslot.m_gary_Itemslot_Etc[i] = null;
-                                }
-                                //Debug.Log("[" + i + "] != null");
-                            }
-                            else
-                            {
-                                m_gary_Itemslot[i].GetComponent<Itemslot>().SetNull();
-                                //Debug.Log("[" + i + "] == null");
+                                Player_Itemslot.m_gary_Itemslot_Equip[i] = null;
                             }
                         }
+                        else
+                            m_gary_Itemslot[i].GetComponent<Itemslot>().SetNull();
                     }
-                    break;
-            }
+                }
+                break;
+            case E_ITEMSLOT.USE:
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+                        if (Player_Itemslot.m_nary_Itemslot_Use_Count[i] != 0)
+                        {
+                            m_gary_Itemslot[i].GetComponent<Itemslot>().SetItem_Use(Player_Itemslot.m_gary_Itemslot_Use[i], Player_Itemslot.m_nary_Itemslot_Use_Count[i]);
+                            if (Player_Itemslot.m_nary_Itemslot_Use_Count[i] == 0)
+                            {
+                                Player_Itemslot.m_gary_Itemslot_Use[i] = null;
+                            }
+                        }
+                        else
+                            m_gary_Itemslot[i].GetComponent<Itemslot>().SetNull();
+                    }
+                }
+                break;
+            case E_ITEMSLOT.ETC:
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+                        if (Player_Itemslot.m_nary_Itemslot_Etc_Count[i] != 0)
+                        {
+                            m_gary_Itemslot[i].GetComponent<Itemslot>().SetItem_Etc(Player_Itemslot.m_gary_Itemslot_Etc[i], Player_Itemslot.m_nary_Itemslot_Etc_Count[i]);
+                            if (Player_Itemslot.m_nary_Itemslot_Etc_Count[i] == 0)
+                            {
+                                Player_Itemslot.m_gary_Itemslot_Etc[i] = null;
+                            }
+                            //Debug.Log("[" + i + "] != null");
+                        }
+                        else
+                        {
+                            m_gary_Itemslot[i].GetComponent<Itemslot>().SetNull();
+                            //Debug.Log("[" + i + "] == null");
+                        }
+                    }
+                }
+                break;
         }
     }
+    // 인벤토리 GUI 업데이트(골드(재화))
     public void UpdateItemslot_Gold()
     {
         if (m_gPanel_Itemslot.activeSelf == true)
@@ -203,64 +261,16 @@ public class GUI_Itemslot : MonoBehaviour
         }
     }
 
-    // 아이템 슬롯 초기화.
+    // 인벤토리 관련 GUI 업데이트(초기화)
     void Itemslot_Reset()
     {
         m_gScrollbarVertical_Itemslot_Content_Slot.GetComponent<Scrollbar>().value = 1;
+        
         GUIManager_Total.Instance.m_GUI_Itemslot_Equip_Information.Init_Scrollbar();
         GUIManager_Total.Instance.m_GUI_Itemslot_Equip_Information.m_gPanel_Itemslot_Equip_Information.SetActive(false);
         GUIManager_Total.Instance.m_GUI_Itemslot_Use_Information.Init_Scrollbar();
         GUIManager_Total.Instance.m_GUI_Itemslot_Use_Information.m_gPanel_Itemslot_Use_Information.SetActive(false);
         GUIManager_Total.Instance.m_GUI_Itemslot_Etc_Information.Init_Scrollbar();
         GUIManager_Total.Instance.m_GUI_Itemslot_Etc_Information.m_gPanel_Itemslot_Etc_Information.SetActive(false);
-    }
-
-    // 버튼 이벤트 처리
-    public void Btn_Press_Exit()
-    {
-        m_BTN_Itemslot_Exit.GetComponent<Image>().color = new Color(.75f, .75f, .75f, 1);
-        //m_gPanel_Itemslot_Equip_Information.SetActive(false);
-
-        Display_GUI_Itemslot();
-        GUIManager_Total.Instance.Delete_GUI_Priority(8);
-    }
-    public void Btn_Press_Itemslot_Equip()
-    {
-        m_eItemslot = E_ITEMSLOT.EQUIP;
-
-        UpdateItemslot();
-        Itemslot_Reset();
-
-        m_BTN_Itemslot_Content_Category_Equip.GetComponent<Image>().color = new Color(.75f, .75f, .75f, 1);
-        m_BTN_Itemslot_Content_Category_Use.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-        m_BTN_Itemslot_Content_Category_Etc.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-
-        m_gPanel_Itemslot.transform.SetAsLastSibling();
-    }
-    public void Btn_Press_Itemslot_Use()
-    {
-        m_eItemslot = E_ITEMSLOT.USE;
-
-        UpdateItemslot();
-        Itemslot_Reset();
-
-        m_BTN_Itemslot_Content_Category_Equip.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-        m_BTN_Itemslot_Content_Category_Use.GetComponent<Image>().color = new Color(.75f, .75f, .75f, 1);
-        m_BTN_Itemslot_Content_Category_Etc.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-
-        m_gPanel_Itemslot.transform.SetAsLastSibling();
-    }
-    public void Btn_Press_Itemslot_Etc()
-    {
-        m_eItemslot = E_ITEMSLOT.ETC;
-
-        UpdateItemslot();
-        Itemslot_Reset();
-
-        m_BTN_Itemslot_Content_Category_Equip.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-        m_BTN_Itemslot_Content_Category_Use.GetComponent<Image>().color = new Color(1, 1, 1, 1);
-        m_BTN_Itemslot_Content_Category_Etc.GetComponent<Image>().color = new Color(.75f, .75f, .75f, 1);
-
-        m_gPanel_Itemslot.transform.SetAsLastSibling();
     }
 }
